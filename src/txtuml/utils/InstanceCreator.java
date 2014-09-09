6b2d27704d883e5bcf61533228dd18e0b1ebe64b
Gergely Dévai <deva@caesar.elte.hu>
2014-09-09 21:28:25 +0000

    txtUML, trunk: tweaking with 'createInstance'

--- src/txtuml/utils/InstanceCreator.java
+++ src/txtuml/utils/InstanceCreator.java
@@ -23,17 +23,20 @@ public class InstanceCreator {
 				} else {
 					param = createInstance(paramTypes[i], depth-1);
 				}
-				if(param == null) {
-					break;
-				}
 				params[i] = param;
 			}
-			if(paramTypes.length == params.length) {
-				try {
-					ret = (T)ctor.newInstance(params);
-				} catch(Exception e) {
-					e.printStackTrace();
-				}
+			// TODO: Do not allow the creation of the new instance
+			// if parameters were not created successfully.
+			// Currently this cannot be fixed:
+			//   When importing a 'new' operation on a model class,
+			//   its constructor requires the enclosing class (the model),
+			//   but 'givenParameters' is not filled in...
+			//   As a result, trying to fix this TODO makes the examples fail.
+			try {
+				ret = (T)ctor.newInstance(params);
+			} catch(Exception e) {
+				// Using this ctor failed, 'ret' is null,
+				// loop again if there are more ctors.
 			}
 			if(ret != null) {
 				break;
