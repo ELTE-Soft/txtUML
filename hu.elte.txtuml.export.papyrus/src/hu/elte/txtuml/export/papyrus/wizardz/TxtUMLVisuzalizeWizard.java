2ba649036731504d93761129750cc104eae528eb
András Dobreff <dobreffandras@gmail.com>
2015-03-26 09:05:20 +0000

    Export Launcher deleted. Calling UML2 export directly from wizard.

--- hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/wizardz/TxtUMLVisuzalizeWizard.java
+++ hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/wizardz/TxtUMLVisuzalizeWizard.java
@@ -1,31 +1,15 @@
 package hu.elte.txtuml.export.papyrus.wizardz;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.List;
-
-import hu.elte.txtuml.export.papyrus.ExportLauncher;
 import hu.elte.txtuml.export.papyrus.MainAction;
 import hu.elte.txtuml.export.papyrus.ProjectManager;
 import hu.elte.txtuml.export.papyrus.preferences.PreferencesManager;
-import hu.elte.txtuml.layout.export.DiagramExportationReport;
-import hu.elte.txtuml.layout.export.DiagramExporter;
-import hu.elte.txtuml.layout.lang.Diagram;
+import hu.elte.txtuml.export.uml2.UML2;
 
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.CommonPlugin;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.JavaCore;
-import org.eclipse.jdt.launching.JavaRuntime;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.dialogs.MessageDialog;
@@ -67,11 +51,12 @@ public class TxtUMLVisuzalizeWizard extends Wizard{
 		final String txtUMLExportProjectname = "hu.elte.txtuml.export.uml2"; // TODO Place in preferences
 		String txtUMLExport =  txtUMLExportProjectname+".UML2";
 		
+		
+		/******** Diagram Layout ************
 		try {
 			Class<?> cls;
 
 			
-			/**/
 			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(txtUMLProjectName);
 			IJavaProject javaproject = JavaCore.create(project);
 			
@@ -89,10 +74,11 @@ public class TxtUMLVisuzalizeWizard extends Wizard{
 			URL[] urls = (URL[]) urlList.toArray(new URL[urlList.size()]);
 			URLClassLoader classLoader = new URLClassLoader(urls, parentClassLoader);
 			
+		
+			
 			
 			cls = classLoader.loadClass(txtUMLLayoutName);
 			classLoader.close();
-			
 
 			@SuppressWarnings("unchecked")
 			Class<? extends Diagram> diagramClass = (Class<? extends Diagram>) cls;
@@ -109,56 +95,54 @@ public class TxtUMLVisuzalizeWizard extends Wizard{
 			e1.printStackTrace();
 		}
 		
+		/***********************************/
+		
 		preferncesManager.setValue(PreferencesManager.TXTUML_VISUALIZE_TXTUML_PROJECT, txtUMLProjectName);
 		preferncesManager.setValue(PreferencesManager.TXTUML_VISUALIZE_TXTUML_MODEL, txtUMLModelName);
 		preferncesManager.setValue(PreferencesManager.TXTUML_VISUALIZE_TXTUML_LAYOUT, txtUMLLayoutName);
 	
-		Runnable visualize = new Runnable() {
-		    @Override
-		    public void run() {
-		    	
-		    	
-		    	URI umlFileURI = URI.createFileURI(txtUMLExportProjectname+"/"+folder+"/"+txtUMLModelName+".uml");
-		    	URI UmlFileResURI = CommonPlugin.resolve(umlFileURI);
-		    	IFile UmlFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(UmlFileResURI.toFileString()));
-		    	
-		    	URI diFileURI = URI.createFileURI(txtUMLModelName+"/"+txtUMLModelName+".di");
-		    	URI diFileResURI = CommonPlugin.resolve(diFileURI);
-		    	IFile diFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(diFileResURI.toFileString()));
-		    	
-		    	IEditorInput input = new FileEditorInput(diFile);
-
-
-		    	IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findEditor(input);
-		    	if(editor != null){
-		    		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().closeEditor(editor, false);
+		
+		/*********  Export *************/
+		    	try {
+					UML2.exportModel(txtUMLModelName, folder);
+				} catch (Exception e) {
+					IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					MessageDialog.openInformation(window.getShell(),"txtUML export Error",e.getMessage());
+					e.printStackTrace();
+					return false;
+				}
+	    /***********************************/
+		/********* Visualization ***********/
+		    	try{
+			    	URI umlFileURI = URI.createFileURI(txtUMLExportProjectname+"/"+folder+"/"+txtUMLModelName+".uml");
+			    	URI UmlFileResURI = CommonPlugin.resolve(umlFileURI);
+			    	IFile UmlFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(UmlFileResURI.toFileString()));
+			    	
+			    	URI diFileURI = URI.createFileURI(txtUMLModelName+"/"+txtUMLModelName+".di");
+			    	URI diFileResURI = CommonPlugin.resolve(diFileURI);
+			    	IFile diFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(diFileResURI.toFileString()));
+			    	
+			    	IEditorInput input = new FileEditorInput(diFile);
+	
+	
+			    	IEditorPart editor = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().findEditor(input);
+			    	if(editor != null){
+			    		PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().closeEditor(editor, false);
+			    	}
+	
+			    	ProjectManager projectManager = new ProjectManager();
+					projectManager.deleteProjectbyName(txtUMLModelName);
+	
+			        MainAction ma  = new MainAction(txtUMLModelName, txtUMLModelName, UmlFile.getRawLocationURI().toString());
+					IAction act = new Action() {};
+					ma.run(act);
+		    	}catch(Exception e){
+		    		IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+					MessageDialog.openInformation(window.getShell(),"txtUML visualization Error",e.getMessage());
+					e.printStackTrace();
+					return false;
 		    	}
-
-		    	ProjectManager projectManager = new ProjectManager();
-				projectManager.deleteProjectbyName(txtUMLModelName);
-		    	
-		        MainAction ma  = new MainAction(txtUMLModelName, txtUMLModelName, UmlFile.getRawLocationURI().toString());
-				IAction act = new Action() {};
-				ma.run(act);
-		    }
-		};
-		
-		try{
-			ExportLauncher launcher = new ExportLauncher();
-			launcher.setExport(txtUMLExport);
-			launcher.setExportProjectName(txtUMLExportProjectname);
-			launcher.setFolderName(folder);
-			launcher.setModelName(txtUMLModelName);
-			launcher.setProjectName(txtUMLProjectName);
-			launcher.setUpConfiguration();
-			launcher.setAfterFinnish(visualize);
-			launcher.run();
-		}catch(CoreException e){
-			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
-			MessageDialog.openInformation(window.getShell(),"Launch Configuration Error",e.getMessage());
-			e.printStackTrace();
-		}
-		
+		/***********************************/
 		return true;
 	}
 
