f189bed3b6a46d8d582f29177631d504a5ac683a
András Dobreff <dobreffandras@gmail.com>
2015-03-31 23:12:45 +0000

    Setting up txtUML Project: - project Natures added - folders (src, bin, META-INF) created - execution enviroment set (javaSE-1.8) - source and output folders added - plugin dependencies added - plugin dependency attribute (org.eclipse.ajdt.aspectpath) added

--- hu.elte.txtuml.project/src/hu/elte/txtuml/project/ProjectCreator.java
+++ hu.elte.txtuml.project/src/hu/elte/txtuml/project/ProjectCreator.java
@@ -1,12 +1,39 @@
 package hu.elte.txtuml.project;
 
+import hu.elte.txtuml.utils.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jdt.core.IAccessRule;
+import org.eclipse.jdt.core.IClasspathAttribute;
+import org.eclipse.jdt.core.IClasspathEntry;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.launching.JavaRuntime;
+import org.eclipse.jdt.launching.environments.IExecutionEnvironment;
+import org.eclipse.jdt.launching.environments.IExecutionEnvironmentsManager;
 
 public class ProjectCreator {
+	
+	public static class ProjectSettings{
+		public String executionEnviromentID;
+		public IFolder output;
+		public IFolder source;
+		public List<Pair<String, String>> pluginDepAttributes;
+	}
+	
 	public static IProject createProject(String name) throws CoreException{
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		final IProject project = root.getProject(name);
@@ -23,13 +50,55 @@ public class ProjectCreator {
 		project.open(new NullProgressMonitor());
 	}
 	
-	public void deleteProject(IProject project) throws CoreException{
+	public static void deleteProject(IProject project) throws CoreException{
 		project.delete(true, true, new NullProgressMonitor());
 	}
 
-	public void deleteProjectbyName(String projectname) throws CoreException{
+	public static void deleteProjectbyName(String projectname) throws CoreException{
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		IProject project = root.getProject(projectname);
 		deleteProject(project);
 	}
+	
+	public static IFolder createFolder(IContainer parent, String projectName) throws CoreException{
+		IFolder folder = parent.getFolder(new Path(projectName));
+		folder.create(true,  true,  new NullProgressMonitor());
+		return folder;
+	}
+
+	public static void addProjectNatures(IProject project, String[] natures) throws CoreException {
+		IProjectDescription description = project.getDescription();
+		description.setNatureIds(natures);
+		project.setDescription(description, new NullProgressMonitor());
+	}
+
+	public static void addProjectSettings(IProject project, ProjectSettings settings) throws JavaModelException {
+		IJavaProject javaProject = JavaCore.create(project);
+		
+		List<IClasspathEntry> entries = new ArrayList<IClasspathEntry>();
+		IExecutionEnvironmentsManager executionEnvironmentsManager = JavaRuntime.getExecutionEnvironmentsManager();
+		IExecutionEnvironment[] executionEnvironments = executionEnvironmentsManager.getExecutionEnvironments();
+		for (IExecutionEnvironment iExecutionEnvironment : executionEnvironments) {
+		    if (iExecutionEnvironment.getId().equals(settings.executionEnviromentID)) {
+		        entries.add(JavaCore.newContainerEntry(JavaRuntime.newJREContainerPath(iExecutionEnvironment)));
+		        break;
+		    }
+		}
+		
+		IPackageFragmentRoot packageRoot = javaProject.getPackageFragmentRoot(settings.source);
+		
+		IClasspathAttribute atts[] = new IClasspathAttribute[settings.pluginDepAttributes.size()];
+		for(int i = 0; i<settings.pluginDepAttributes.size(); i++){
+			Pair<String, String> attribute = settings.pluginDepAttributes.get(i);
+			atts[i] = JavaCore.newClasspathAttribute(attribute.getKey(), attribute.getValue());
+		}
+		
+		IClasspathEntry plugindependences = JavaCore.newContainerEntry(new Path("org.eclipse.pde.core.requiredPlugins"),new IAccessRule[]{}, atts, false);
+		entries.add(plugindependences);
+		entries.add(JavaCore.newSourceEntry(packageRoot.getPath()));
+		javaProject.setRawClasspath(entries.toArray(new IClasspathEntry[entries.size()]), null);
+		javaProject.setOutputLocation(settings.output.getFullPath(), null);
+	}
+	
+	
 }
--- hu.elte.txtuml.project/src/hu/elte/txtuml/project/wizards/TxtUMLProject.java
+++ hu.elte.txtuml.project/src/hu/elte/txtuml/project/wizards/TxtUMLProject.java
@@ -1,9 +1,18 @@
 package hu.elte.txtuml.project.wizards;
 
-import hu.elte.txtuml.project.ProjectCreator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import hu.elte.txtuml.project.ProjectCreator;
+import hu.elte.txtuml.project.ProjectCreator.ProjectSettings;
+import hu.elte.txtuml.utils.Pair;
+
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -57,10 +66,27 @@ public class TxtUMLProject extends Wizard implements INewWizard {
 	
 	private boolean setUptxtUMLProject(String projectName,String modelName){
 		try{
-			System.out.println(projectName);
-			System.out.println(modelName);
 			IProject project = ProjectCreator.createProject(projectName);
 			ProjectCreator.openProject(project);
+			
+			String[] natures = new String[]{JavaCore.NATURE_ID, 
+					"org.eclipse.ajdt.ui.ajnature",
+					"org.eclipse.pde.PluginNature"};
+			ProjectCreator.addProjectNatures(project, natures);
+			
+			IFolder metainf = ProjectCreator.createFolder(project, "META-INF");
+			IFolder src = ProjectCreator.createFolder(project, "src");
+			IFolder bin = ProjectCreator.createFolder(project, "bin");
+			
+			ProjectSettings settings = new ProjectSettings();
+			settings.executionEnviromentID = "JavaSE-1.8";
+			settings.output = bin;
+			settings.source = src;
+			settings.pluginDepAttributes = new ArrayList<Pair<String,String>>();
+			settings.pluginDepAttributes.add(new Pair<String,String>("org.eclipse.ajdt.aspectpath", "org.eclipse.ajdt.aspectpath"));
+			
+			ProjectCreator.addProjectSettings(project, settings);
+			
 		}catch(CoreException e){
 			IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 			MessageDialog.openInformation(window.getShell(),"txtUML Project creation Error",e.getClass()+":\n"+e.getMessage());
