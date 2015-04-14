87a1d62a9113207691bed8525fe3fc7e0ad276e3
András Dobreff <dobreffandras@gmail.com>
2015-04-14 13:20:53 +0000

    Profile also copied to the new project

--- hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/PapyrusModelCreator.java
+++ hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/PapyrusModelCreator.java
@@ -5,14 +5,29 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+
+import java.nio.file.StandardCopyOption;
+
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.CommonPlugin;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.impl.BasicEObjectImpl;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.papyrus.infra.core.resource.ModelMultiException;
 import org.eclipse.papyrus.infra.core.resource.ModelSet;
@@ -20,7 +35,13 @@ import org.eclipse.papyrus.infra.core.sasheditor.contentprovider.IPageManager;
 import org.eclipse.papyrus.infra.core.services.ExtensionServicesRegistry;
 import org.eclipse.papyrus.infra.core.services.ServiceException;
 import org.eclipse.papyrus.infra.core.services.ServicesRegistry;
+import org.eclipse.papyrus.uml.diagram.activity.providers.UMLModelingAssistantProvider;
 import org.eclipse.papyrus.uml.diagram.wizards.category.NewPapyrusModelCommand;
+import org.eclipse.uml2.common.util.UML2Util;
+import org.eclipse.uml2.uml.Model;
+import org.eclipse.uml2.uml.UMLFactory;
+import org.eclipse.uml2.uml.Profile;
+import org.eclipse.uml2.uml.UMLPackage;
 
 /**
  * Controls the creation of a Papyrus Model
@@ -54,10 +75,10 @@ public class PapyrusModelCreator {
 	 * Initializes the instance	
 	 * @param modelpath - The path of the Papyrus model (at the end is the name of the Papyrus model)
 	 * @param sourceUMLPath - The path of the source .uml File
-	 * @throws FileNotFoundException
 	 * @throws CoreException
+	 * @throws IOException 
 	 */
-	public void init(String modelpath, String sourceUMLPath) throws FileNotFoundException, CoreException {
+	public void init(String modelpath, String sourceUMLPath) throws CoreException, IOException {
 		diFilePath = modelpath+".di";
 		umlFilePath = modelpath+".uml";
 		
@@ -70,15 +91,46 @@ public class PapyrusModelCreator {
 	
 	/**
 	 * Copies the content of the sourceUMLPath to the umlFile
+	 * The referenced Profile files will be also copied
 	 * @param sourceUMLPath - The path of the source .uml File
-	 * @throws FileNotFoundException
 	 * @throws CoreException
+	 * @throws IOException 
 	 */
-	private void setUpUMLFile(String sourceUMLPath) throws FileNotFoundException, CoreException {
-			InputStream  is = new FileInputStream(new File(java.net.URI.create(sourceUMLPath)));
-			umlFile.create(null, true, null);
-			umlFile.setContents(is, true, false, new NullProgressMonitor());
+	private void setUpUMLFile(String sourceUMLPath) throws CoreException, IOException {
+			copyFile(sourceUMLPath, umlFile);
+			
+			Model m = loadModel(URI.createPlatformResourceURI(umlFilePath, false));
+			EList<Profile> profiles = m.getAllAppliedProfiles();
+			for(Profile profile : profiles){
+						String filestring = ((BasicEObjectImpl) profile).eProxyURI().toPlatformString(false);
+						Path profileFilepath = new Path(filestring);
+						
+						String oldFolder = new Path(new Path(sourceUMLPath).toFile().getParent().toString()).toString();
+						String oldFileName = oldFolder+Path.SEPARATOR+profileFilepath.toFile().getName();
+						
+						int index = umlFilePath.lastIndexOf(Path.SEPARATOR);
+						String newFileName = umlFilePath.substring(0, index)+Path.SEPARATOR+profileFilepath.toFile().getName();
+						IFile newFile = fileFromPath(newFileName);
+						
+						copyFile(oldFileName, newFile);	
+			}
 	}
+	
+	private Model loadModel(URI uri) {
+		ResourceSetImpl RESOURCE_SET = new ResourceSetImpl();
+		Resource resource = RESOURCE_SET.getResource(uri, true);
+
+		Model model = (Model) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.MODEL);
+		return model;
+	}
+	
+	private void copyFile(String sourcepath, IFile newFile) throws FileNotFoundException, CoreException{
+		File oldFile = new File(java.net.URI.create(sourcepath));
+		FileInputStream is2 = new FileInputStream(oldFile);
+		newFile.create(null, true, null);
+		newFile.setContents(is2, true, false, new NullProgressMonitor());
+	}
+
 	
 	/**
 	 * Creates an empty Papyrus Model
