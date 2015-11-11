d0b0f030c4d1167c766c74daf8bb253901f5564c
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-11-11 02:24:57 +0100

    New test included in AllTests of api.model.tests.

--- tests/hu.elte.txtuml.api.model.tests/src/hu/elte/txtuml/api/model/tests/AllTests.java
+++ tests/hu.elte.txtuml.api.model.tests/src/hu/elte/txtuml/api/model/tests/AllTests.java
@@ -1,5 +1,6 @@
 package hu.elte.txtuml.api.model.tests;
 
+import hu.elte.txtuml.api.model.AllPackagePrivateTests;
 import hu.elte.txtuml.api.model.tests.assocends.AllAssociationEndTests;
 import hu.elte.txtuml.api.model.tests.error.deletion.AllDeletionErrorTests;
 import hu.elte.txtuml.api.model.tests.error.multiplicity.AllAssociationEndErrorTests;
@@ -13,9 +14,9 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ AllShutdownTests.class, AllAssociationEndTests.class,
-		AllStateMachineTests.class, AllDeletionErrorTests.class,
-		AllStateMachineErrorTests.class, AllAssociationEndErrorTests.class,
-		AllOtherErrorTests.class })
+@SuiteClasses({ AllPackagePrivateTests.class, AllShutdownTests.class,
+		AllAssociationEndTests.class, AllStateMachineTests.class,
+		AllDeletionErrorTests.class, AllStateMachineErrorTests.class,
+		AllAssociationEndErrorTests.class, AllOtherErrorTests.class })
 public class AllTests {
 }
