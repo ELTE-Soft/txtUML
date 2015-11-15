d87d86ecd7915539c567eee8712842fb8506da6a
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-11-15 17:59:36 +0100

    Added split version of Machine3 example.

---
+++ examples/hu.elte.txtuml.examples.machine/src/hu/elte/txtuml/examples/machine/model3/User.java
@@ -0,0 +1,150 @@
+package hu.elte.txtuml.examples.machine.model3;
+
+import hu.elte.txtuml.api.model.Action;
+import hu.elte.txtuml.api.model.Collection;
+import hu.elte.txtuml.api.model.From;
+import hu.elte.txtuml.api.model.ModelClass;
+import hu.elte.txtuml.api.model.To;
+import hu.elte.txtuml.api.model.Trigger;
+import hu.elte.txtuml.api.stdlib.Timer;
+import hu.elte.txtuml.examples.machine.model3.associations.Usage;
+import hu.elte.txtuml.examples.machine.model3.signals.ButtonPress;
+import hu.elte.txtuml.examples.machine.model3.signals.DoTasks;
+import hu.elte.txtuml.examples.machine.model3.signals.DoYourWork;
+
+public class User extends ModelClass {
+
+	String name;
+	int id;
+	int workToDo = 7;
+
+	class Init extends Initial {
+	}
+
+	class NotWorking extends State {
+	}
+
+	class WhereToGo extends Choice {
+	}
+
+	@From(Init.class)
+	@To(NotWorking.class)
+	class Initialize extends Transition {
+		@Override
+		public void effect() {
+			Action.log("\t" + name.toString() + ": initializing...");
+		}
+	}
+
+	@From(NotWorking.class)
+	@To(WhereToGo.class)
+	@Trigger(DoYourWork.class)
+	class NW_WTG extends Transition {
+	}
+
+	@From(WhereToGo.class)
+	@To(NotWorking.class)
+	// from choice (trigger is not needed)
+	class NoWork extends Transition {
+		@Override
+		public boolean guard() {
+			return workToDo == 0;
+		}
+
+		@Override
+		public void effect() {
+			Action.log("\t" + name.toString() + ": I have no work to do!");
+		}
+	}
+
+	@From(WhereToGo.class)
+	@To(NotWorking.class)
+	// from choice (trigger is not needed)
+	class HasWork extends Transition {
+		@Override
+		public boolean guard() {
+			return Else();
+		}
+
+		@Override
+		public void effect() {
+			Action.log("\t" + name.toString() + ": I am doing my work now...");
+			doWork();
+		}
+	}
+
+	User findOtherUser(Machine m) {
+
+		Action.log("\t" + name.toString() + ": analyzing machine...");
+
+		Collection<User> usersOfM = m.assoc(Usage.userOfMachine.class);
+
+		Action.log("\t\tI found its users!");
+		Action.log("");
+
+		usersOfM.forEach(user -> user.sayHello());
+		Action.log("");
+
+		if (usersOfM.contains(this)) {
+			Action.log("\t\tI am a user of this machine.");
+		}
+
+		if (usersOfM.selectAll(user -> user.id == 0).isEmpty()) {
+			Action.log("\t\tNo user of this machine has an id of 0.");
+		}
+
+		Collection<User> otherUsers = usersOfM.remove(this);
+
+		if (otherUsers.count() == 1) {
+			Action.log("\t\tThere is exactly one other person who is user of this machine.");
+		}
+
+		return otherUsers.selectAny();
+	}
+
+	void sayHello() {
+		Action.log("\t\tHello, I am " + name.toString() + ".");
+	}
+
+	void newWork() {
+		++workToDo;
+		Action.log("\t" + name.toString() + ": I got some new work to do.");
+	}
+
+	void workDone() {
+		--workToDo;
+		Action.log("\t" + name.toString() + ": I am done with some of my work.");
+	}
+
+	void doWork() {
+		Action.log("\t" + name.toString() + ": starting to work...");
+
+		Machine myMachine = this.assoc(Usage.usedMachine.class).selectAny();
+
+		User otherUser = findOtherUser(myMachine);
+
+		Action.log("\t" + name.toString()
+				+ ": giving some of my work to other user...");
+
+		for (int i = 0; i < 3; ++i) {
+			Action.log("\t" + name.toString() + ": giving work to other user (" + i
+					+ ")");
+			otherUser.newWork();
+			this.workDone();
+		}
+
+		Action.send(myMachine, new ButtonPress());
+		// Switching the machine on.
+
+		Action.log("\t" + name.toString() + ": finishing my work...");
+
+		Action.send(myMachine, new DoTasks(workToDo));
+
+		Timer.start(myMachine, new ButtonPress(), 2000);
+		// Switching off the machine with some delay.
+
+		Action.log("\t" + name.toString() + ": work finished...");
+
+	}
+
+}
