9fbd731d9b9f1ada54aeb33a9e2a307d141e6f3c
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-11-15 18:08:15 +0100

    Updated testers and diagrams of the 3 machine models.

--- examples/hu.elte.txtuml.examples.machine/src/hu/elte/txtuml/examples/machine/Machine1.java
+++ examples/hu.elte.txtuml.examples.machine/src/hu/elte/txtuml/examples/machine/Machine1.java
@@ -1,116 +1,13 @@
 package hu.elte.txtuml.examples.machine;
 
-import hu.elte.txtuml.api.model.*;
-import hu.elte.txtuml.examples.machine.Machine1Model.*;
+import hu.elte.txtuml.api.model.Action;
+import hu.elte.txtuml.api.model.ModelExecutor;
+import hu.elte.txtuml.examples.machine.model1.Machine;
+import hu.elte.txtuml.examples.machine.model1.User;
+import hu.elte.txtuml.examples.machine.model1.associations.Usage;
+import hu.elte.txtuml.examples.machine.model1.signals.DoYourWork;
 
-class Machine1Model extends Model {
-
-	// classes
-	
-	class Machine extends ModelClass {
-
-		class Init extends Initial {}
-
-		class Off extends State {
-			@Override
-			public void entry() {
-				Action.log("\tMachine enters state: 'off'");
-			}
-
-			@Override
-			public void exit() {
-				Action.log("\tMachine exits state: 'off'");
-			}
-		}
-
-		class On extends State {
-			@Override
-			public void entry() {
-				Action.log("\tMachine enters state: 'on'");
-			}
-
-			@Override
-			public void exit() {
-				Action.log("\tMachine exits state: 'on'");
-			}
-
-		}
-
-		@From(Init.class) @To(Off.class)
-		class Initialize extends Transition {
-			@Override
-			public void effect() {
-				Action.log("\tMachine: initializing...");
-			}
-		}
-
-		@From(Off.class) @To(On.class) @Trigger(ButtonPress.class)
-		class SwitchOn extends Transition {
-			@Override
-			public void effect() {
-				Action.log("\tMachine: switching on...");
-			}
-
-		}
-
-		@From(On.class) @To(Off.class) @Trigger(ButtonPress.class)
-		class SwitchOff extends Transition {
-			@Override
-			public void effect() {
-				Action.log("\tMachine: switching off...");
-			}
-		}
-	}
-
-	class User extends ModelClass {
-		
-		class Init extends Initial {}
-		class Ready extends State {}
-		
-		@From(Init.class) @To(Ready.class)
-		class Initialize extends Transition {
-			@Override
-			public void effect() {
-				Action.log("\tUser: initializing...");				
-			}
-		}
-
-		@From(Ready.class) @To(Ready.class) @Trigger(DoYourWork.class)
-		class Working extends Transition {
-			@Override
-			public void effect() {
-				Action.log("\tUser: working...");
-				doWork();
-			}
-		}
-		
-		void doWork() {
-			Action.log("\tUser: starting to work...");
-			Machine myMachine = this.assoc(Usage.usedMachine.class).selectAny();			
-			Action.send(myMachine, new ButtonPress());
-			Action.send(myMachine, new ButtonPress());
-			Action.send(myMachine, new ButtonPress());
-			Action.log("\tUser: work finished...");
-		}
-	}
-
-	// associations
-	
-	class Usage extends Association {
-		class usedMachine extends One<Machine> {}
-		class userOfMachine extends HiddenMany<User> {}
-	}
-
-	// signals
-	
-	static class ButtonPress extends Signal {}
-
-	static class DoYourWork extends Signal {}
-	// Signal classes are allowed to be static for simpler use.
-	
-}
-
-class Machine1Tester {
+public class Machine1 {
 
 	void test() {
 		ModelExecutor.Settings.setExecutorLog(true);
@@ -132,10 +29,8 @@ class Machine1Tester {
 		ModelExecutor.shutdown();
 	}
 
-}
-
-public class Machine1 {
 	public static void main(String[] args) {
-		new Machine1Tester().test();
+		new Machine1().test();
 	}
+
 }
