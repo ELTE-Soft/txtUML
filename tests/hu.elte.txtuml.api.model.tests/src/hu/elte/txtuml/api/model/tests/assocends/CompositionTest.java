ca12f4e63834aa77e365ce73c60a3e337113567f
nboldi <nboldi@elte.hu>
2015-11-16 12:57:11 +0100

    Compositions working and exported... without javadoc

---
+++ tests/hu.elte.txtuml.api.model.tests/src/hu/elte/txtuml/api/model/tests/assocends/CompositionTest.java
@@ -0,0 +1,39 @@
+package hu.elte.txtuml.api.model.tests.assocends;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hu.elte.txtuml.api.model.Action;
+import hu.elte.txtuml.api.model.Composition;
+import hu.elte.txtuml.api.model.Container;
+import hu.elte.txtuml.api.model.ModelClass;
+import hu.elte.txtuml.api.model.tests.assocends.CompositionTest.MyComposition.MyContainer;
+import hu.elte.txtuml.api.model.tests.assocends.CompositionTest.MyComposition.MyPart;
+
+public class CompositionTest {
+
+	class A extends ModelClass {
+	}
+
+	class B extends ModelClass {
+	}
+
+	class MyComposition extends Composition {
+		class MyContainer extends Container<A> {
+		}
+
+		class MyPart extends Many<B> {
+		}
+	}
+	
+	@Test
+	public void tryComposition() throws Exception {
+		A a = new A();
+		Action.link(MyContainer.class, a, MyPart.class, new B());
+		Action.link(MyContainer.class, a, MyPart.class, new B());
+		Action.link(MyContainer.class, a, MyPart.class, new B());
+		assertEquals(3, a.assoc(MyPart.class).count());
+	}
+
+}
