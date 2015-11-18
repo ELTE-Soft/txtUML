5e0342fc6cc73be4b8ea69b5044b78cd1742220f
kmate <k_mate@inf.elte.hu>
2015-11-18 17:13:48 +0100

    Machine examples updated according to XtxtUML split models support

--- examples/hu.elte.txtuml.examples.machine/src/hu/elte/txtuml/examples/machine/XMachine2Diagram.java
+++ examples/hu.elte.txtuml.examples.machine/src/hu/elte/txtuml/examples/machine/XMachine2Diagram.java
@@ -1,7 +1,9 @@
 package hu.elte.txtuml.examples.machine;
 
-import hu.elte.txtuml.api.layout.*;
-import hu.elte.txtuml.examples.machine.XMachine2.*;
+import hu.elte.txtuml.api.layout.Diagram;
+import hu.elte.txtuml.api.layout.Right;
+import hu.elte.txtuml.examples.machine.xmachine2.Machine;
+import hu.elte.txtuml.examples.machine.xmachine2.User;
 
 class XMachine2Diagram extends Diagram {
 	@Right(from = Machine.class, val = User.class)
