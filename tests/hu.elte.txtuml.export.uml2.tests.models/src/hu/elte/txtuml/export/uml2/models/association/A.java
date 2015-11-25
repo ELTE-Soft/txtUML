7b90b87b92da558d651f6b0dd94fcb28910ddc27
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-11-25 16:05:28 +0100

    Split models of export.uml2.tests.models.

---
+++ tests/hu.elte.txtuml.export.uml2.tests.models/src/hu/elte/txtuml/export/uml2/models/association/A.java
@@ -0,0 +1,6 @@
+package hu.elte.txtuml.export.uml2.models.association;
+
+import hu.elte.txtuml.api.model.ModelClass;
+
+public class A extends ModelClass {
+}
---
+++ tests/hu.elte.txtuml.export.uml2.tests.models/src/hu/elte/txtuml/export/uml2/models/association/package-info.java
@@ -0,0 +1,4 @@
+@Model
+package hu.elte.txtuml.export.uml2.models.association;
+
+import hu.elte.txtuml.api.model.Model;
