d4e3bcce59d0940ca0679658343c626f454b08e1
Gregorics Balázs <grego.balu@gmail.com>
2015-11-25 17:57:31 +0100

    Added UnitTest.java file.

---
+++ tests/hu.elte.txtuml.layout.visualizer.tests/src/hu/elte/txtuml/layout/visualizer/tests/UnitTests.java
@@ -0,0 +1,23 @@
+package hu.elte.txtuml.layout.visualizer;
+
+import hu.elte.txtuml.layout.visualizer.tests.algorithms.DefaultStatementTest;
+import hu.elte.txtuml.layout.visualizer.tests.algorithms.links.GraphSearchTest;
+import hu.elte.txtuml.layout.visualizer.tests.model.DirectionTest;
+import hu.elte.txtuml.layout.visualizer.tests.model.PointTest;
+import hu.elte.txtuml.layout.visualizer.tests.statements.StatementTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	DefaultStatementTest.class,
+	GraphSearchTest.class,
+	DirectionTest.class,
+	PointTest.class,
+	StatementTest.class
+	})
+public class UnitTests {
+
+}
