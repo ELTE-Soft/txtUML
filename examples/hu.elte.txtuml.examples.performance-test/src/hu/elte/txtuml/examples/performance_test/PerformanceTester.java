8a3633b876b13384e7111f5704811fe159f6d0a5
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-11-26 03:52:28 +0100

    Added split version of performance-test example.

--- examples/hu.elte.txtuml.examples.performance-test/src/hu/elte/txtuml/examples/performance_test/PerformanceTester.java
+++ examples/hu.elte.txtuml.examples.performance-test/src/hu/elte/txtuml/examples/performance_test/PerformanceTester.java
@@ -1,8 +1,11 @@
+package hu.elte.txtuml.examples.performance_test;
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 import hu.elte.txtuml.api.model.Action;
 import hu.elte.txtuml.api.model.ModelExecutor;
+import hu.elte.txtuml.examples.performance_test.model.Test;
 
 public class PerformanceTester {
 
@@ -13,24 +16,23 @@ public class PerformanceTester {
 	}
 
 	public static void main(String[] args) {
-		 ModelExecutor.Settings.setExecutorLog(true);
-		
+		ModelExecutor.Settings.setExecutorLog(true);
+
 		long start = System.currentTimeMillis();
-		
+
 		ModelExecutor.Settings.setDynamicChecks(false);
 
-		PerformanceTestModel_.Test m = Action
-				.create(PerformanceTestModel_.Test.class);
+		Test m = Action.create(Test.class);
 
-//		PerformanceTestModel.Test m = Action
-//				.create(PerformanceTestModel.Test.class);
-		
+		// PerformanceTestModel.Test m = Action
+		// .create(PerformanceTestModel.Test.class);
+
 		m.test();
-		
+
 		ModelExecutor.shutdown();
-		
+
 		System.out.println(System.currentTimeMillis() - start);
-		
+
 	}
 
 }
