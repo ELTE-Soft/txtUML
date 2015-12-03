3e87beb62f357f1a55cca22af18fddfaa3bcdd04
Kovács Gábor Ferenc <kovacsgabor@users.noreply.github.com>
2015-12-03 02:49:13 +0100

    JavaDoc fixes (Composition and Container).

--- plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/Composition.java
+++ plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/Composition.java
@@ -15,7 +15,7 @@ import hu.elte.txtuml.api.model.assocends.Navigability.Navigable;
  * two inner classes which both extend {@link AssociationEnd}. One of these ends
  * must be the container of the members at the other end. The container must be
  * a subclass of <code>Container</code>. The other end can be a subclass of any
- * <code>AssociationEnd</code>
+ * <code>AssociationEnd</code> (but not a <code>Container</code>).
  * <p>
  * The two model classes which the association connects are defined by the two
  * association ends' generic parameters.
@@ -28,9 +28,10 @@ import hu.elte.txtuml.api.model.assocends.Navigability.Navigable;
  * <p>
  * <b>Subtype requirements:</b>
  * <ul>
- * <li>must be the nested class of a subclass of {@link Model}</li>
+ * <li>must be a top level class (not a nested or local class)</li>
  * <li>must have two inner classes which are subclasses of
- * <code>AssociationEnd</code></li>
+ * <code>AssociationEnd</code>, exactly one being a subclass of
+ * <code>Container</code></li>
  * </ul>
  * <p>
  * <b>Subtype restrictions:</b>
@@ -71,10 +72,13 @@ import hu.elte.txtuml.api.model.assocends.Navigability.Navigable;
  */
 public class Composition extends Association {
 
+	/**
+	 * Sole constructor of <code>Composition</code>.
+	 */
 	protected Composition() {
 
 	}
-	
+
 	/**
 	 * Abstract base class for the container end of a composition association.
 	 * 
@@ -87,30 +91,8 @@ public class Composition extends Association {
 	 * The container end should be defined as inner class of a composition (a
 	 * subclass of {@link Composition}).
 	 * <p>
-	 * When asked for the model objects at an association end (with the
-	 * {@link ModelClass#assoc(Class) assoc} method), an instance of the actual
-	 * representing class of that certain association end will be returned. As
-	 * all association ends are implementing the {@link Collection} interface,
-	 * the contained objects might be accessed through the collection methods.
-	 * <p>
-	 * The multiplicity of an association end might only be checked during model
-	 * execution. The upper bound is always checked, if it is ever offended, an
-	 * error message is shown in the model executor's error log (see the
-	 * documentation of the
-	 * {@link ModelExecutor.Settings#setExecutorErrorStream(java.io.PrintStream)
-	 * ModelExecutor.Settings.setExecutorErrorStream} method). However, this
-	 * does not cause the execution to fail. The lower bound might be offended
-	 * temporarily, but has to be restored before the current <i>execution
-	 * step</i> ends. It is checked at the beginning of the next <i>execution
-	 * step</i> and an error message is shown if it is still offended and the
-	 * regarding model object is not in {@link ModelClass.Status#DELETED
-	 * DELETED} status. However, as this check is relatively slow, it might be
-	 * switched off along with other optional checks with the
-	 * {@link ModelExecutor.Settings#setDynamicChecks(boolean)
-	 * ModelExecutor.Settings.setDynamicChecks} method.
-	 * <p>
-	 * See the documentation of {@link Model} for information about execution
-	 * steps.
+	 * Apart from this, container ends should be used like any other association
+	 * end. See the documentation of {@link AssociationEnd} for details.
 	 * 
 	 * <p>
 	 * <b>Java restrictions:</b>
