5a9219197a097dadc0620bfec20f65a080ed7742
Zoltán Gera <gerazo80@gmail.com>
2015-12-21 16:07:18 +0100

    Implement Papyrus marker management by hand

--- plugins/hu.elte.txtuml.diagnostics/src/hu/elte/txtuml/diagnostics/session/Animator.java
+++ plugins/hu.elte.txtuml.diagnostics/src/hu/elte/txtuml/diagnostics/session/Animator.java
@@ -6,8 +6,17 @@ import java.util.Map;
 import hu.elte.txtuml.api.diagnostics.protocol.MessageType;
 import hu.elte.txtuml.api.diagnostics.protocol.ModelEvent;
 
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.papyrus.moka.ui.presentation.AnimationUtils;
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.papyrus.infra.services.markerlistener.IPapyrusMarker;
+import org.eclipse.papyrus.infra.services.markerlistener.PapyrusMarkerAdapter;
 
 /**
  * Animates diagrams
@@ -15,27 +24,28 @@ import org.eclipse.papyrus.moka.ui.presentation.AnimationUtils;
  */
 class Animator {
 	private static final int ANIMATION_TIMER = 1000;
+	private static final String ANIMATED_MARKER_ID = "hu.elte.txtuml.diagnostics.animatedmarker";
+
 	
 	private InstanceRegister instanceRegister;
 	private ModelMapper modelMapper;
-	private AnimationUtils animationUtils;
 	private Map<String, UniqueInstance> classNameToAnimatedInstance = new HashMap<String, UniqueInstance>();
 	private Map<String, EObject> classNameToAnimatedElement = new HashMap<String, EObject>();
+	private Map<EObject, IPapyrusMarker> eobjectToMarker = new HashMap<EObject, IPapyrusMarker>();
 	
 	Animator(InstanceRegister instanceRegister, ModelMapper modelMapper) {
 		this.instanceRegister = instanceRegister;
 		this.modelMapper = modelMapper;
-		animationUtils = AnimationUtils.getInstance();
-		AnimationUtils.init();
 	}
 	
 	void dispose() {
+		removeAllAnimationMarkers();
+		eobjectToMarker.clear();
+		eobjectToMarker = null;
 		classNameToAnimatedElement.clear();
 		classNameToAnimatedElement = null;
 		classNameToAnimatedInstance.clear();
 		classNameToAnimatedInstance = null;
-		animationUtils.removeAllAnimationMarker();
-		animationUtils = null;
 		modelMapper = null;
 		instanceRegister = null;
 	}
@@ -56,9 +66,8 @@ class Animator {
 		removeMarkerFromClass(event.modelClassName);
 		// animationUtils.removeAllAnimationMarker();
 		if (eobject != null) {
-			animationUtils.addAnimationMarker(eobject);
+			addAnimationMarker(eobject);
 			classNameToAnimatedElement.put(event.modelClassName, eobject);
-			
 		}
 
 		try {
@@ -69,7 +78,48 @@ class Animator {
 	private void removeMarkerFromClass(String modelClassName) {
 		EObject animatedObj = classNameToAnimatedElement.get(modelClassName); 
 		if(animatedObj != null) {
-			animationUtils.removeAnimationMarker(animatedObj);
+			removeAnimationMarker(animatedObj);
+		}
+	}
+	
+	private void removeAllAnimationMarkers() {
+		for (EObject eobject : eobjectToMarker.keySet()) {
+			removeAnimationMarker(eobject);
+		}
+	}
+	
+	private void addAnimationMarker(EObject eobject) {
+		Map<String, String> attributes = new HashMap<String, String>();
+		attributes.put(EValidator.URI_ATTRIBUTE, EcoreUtil.getURI(eobject).toString());
+		IResource resource = null;
+		IWorkspace workspace = ResourcesPlugin.getWorkspace(); 
+		if (workspace != null) {
+			resource = workspace.getRoot().getFile(new Path(eobject.eResource().getURI().toPlatformString(true)));
+		}
+		if (resource != null) {
+			IMarker imarker = null;
+			try {
+				imarker = resource.createMarker(ANIMATED_MARKER_ID);
+				imarker.setAttributes(attributes);
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+			if (imarker != null) {
+				IPapyrusMarker marker = PapyrusMarkerAdapter.wrap(eobject.eResource(), imarker, attributes);
+				eobjectToMarker.put(eobject, marker);
+			}
+		}
+	}
+
+	private void removeAnimationMarker(EObject eobject) {
+		IPapyrusMarker marker = eobjectToMarker.get(eobject);
+		if (marker != null) {
+			try {
+				marker.delete();
+			} catch (CoreException e) {
+				e.printStackTrace();
+			}
+			eobjectToMarker.remove(eobject);
 		}
 	}
 
