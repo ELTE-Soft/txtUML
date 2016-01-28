a25f23f871d507fcaeecb08cea96b65259c69317
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2016-01-28 11:06:14 +0100

    Interface.Empty is added.

--- plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/Interface.java
+++ plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/Interface.java
@@ -18,8 +18,11 @@ package hu.elte.txtuml.api.model;
  * {@code Signal} itself). This reception method must also have the name
  * {@code reception} (more precisely, must equal to the constant
  * {@link Interface#RECEPTION_NAME}). This reception will only accept signals
- * that are of a subtype of the given signal type (or all signals if
- * the parameters's type is {@code Signal}).
+ * that are of a subtype of the given signal type (or all signals if the
+ * parameters's type is {@code Signal}).
+ * <p>
+ * For specific use cases, when an empty interface is needed, the
+ * {@link Interface.Empty} type is present in the API.
  * 
  * <p>
  * <b>Java restrictions:</b>
@@ -52,8 +55,7 @@ package hu.elte.txtuml.api.model;
  * See the documentation of {@link hu.elte.txtuml.api.model.Model Model} for an
  * overview on modeling in JtxtUML.
  *
- * @author Gabor Ferenc Kovacs
- *
+ * @see Interface.Empty
  */
 public interface Interface extends ModelElement {
 
@@ -63,5 +65,40 @@ public interface Interface extends ModelElement {
 	 * See the documentation of {@link Interface} for more information.
 	 */
 	public static final String RECEPTION_NAME = "reception";
+
+	/**
+	 * An empty model interface provided in the API for convenience.
+	 * 
+	 * <p>
+	 * <b>Represents:</b> an empty interface
+	 * <p>
+	 * <b>Usage:</b>
+	 * <p>
+	 *
+	 * Can be used whenever an interface is needed in the model, for example, as
+	 * the provided or required interface of a port which means that no
+	 * communication is possible on the specific port in the specific direction
+	 * as this interface is empty, having no receptions.
+	 * <p>
+	 * Using {@code Empty} when an empty interface is needed is <i>not
+	 * mandatory, but <b>recommended</b></i> to support code consistency and
+	 * understandability, unless the interface which is to be used has a special
+	 * meaning in the specific context.
+	 * 
+	 * <p>
+	 * <b>Java restrictions:</b>
+	 * <ul>
+	 * <li><i>Instantiate:</i> disallowed</li>
+	 * <li><i>Define subtype:</i> disallowed</li>
+	 * </ul>
+	 * 
+	 * <p>
+	 * See the documentation of {@link hu.elte.txtuml.api.model.Model Model} for
+	 * an overview on modeling in JtxtUML.
+	 *
+	 */
+	public interface Empty extends Interface {
+
+	}
 
 }
