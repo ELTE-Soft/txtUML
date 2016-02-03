833c162364009c3a6fbf04b778ab88e0e2917e76
Nagy András <nagyandras95@users.noreply.github.com>
2016-02-03 18:36:59 +0100

    Added Java Doc to Configuration and Group classes.

--- plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/description/Configuration.java
+++ plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/description/Configuration.java
@@ -1,5 +1,22 @@
 package hu.elte.txtuml.export.cpp.description;
 
+
+/**
+ * Base class for thread management configuration.
+ * <p>
+ * <b>Example:</b>
+ * <p>
+ * 
+ * <pre>
+ * <code>
+ *{@literal @Group(contains = {A.class,B.class}, max = 10, constant = 5, gradient = 0.1)}
+ *{@literal @Group(contains = {C.class})}
+ * {@literal @Multithreading(false)}
+ * public class C extends Configuration {}
+ * </code>
+ * </pre>
+ */
+
 public abstract class Configuration{
 	
 }
--- plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/description/Group.java
+++ plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/description/Group.java
@@ -11,6 +11,26 @@ import java.lang.annotation.Target;
 import hu.elte.txtuml.api.model.ModelClass;
 import hu.elte.txtuml.export.cpp.description.GroupContainer;
 
+
+/**
+ * <p>
+ * A thread configuration statement witch classifies the classes to the same thread pool and
+ * configures it.
+ * It defines the number of threads according to a linear function 
+ * parameterized by the number of created objects.
+ * The max statement defines how many threads could be at most.
+ * <p>
+ * <p>
+ * <b>Example:</b>
+ * <p>
+ * 
+ * <pre>
+ * <code>
+ * {@literal @Group(contains = {A.class,B.class}, max = 10, constant = 5, gradient = 0.1)}
+ * </code>
+ * </pre>
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Repeatable(GroupContainer.class)
