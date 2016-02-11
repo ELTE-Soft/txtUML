536f7c44de86ecc210f91eb863565873326681ef
nboldi <nboldi@elte.hu>
2016-02-11 17:05:36 +0100

    merged model export changes from master

--- plugins/hu.elte.txtuml.export.uml2/src/hu/elte/txtuml/export/uml2/UML2.java
+++ plugins/hu.elte.txtuml.export.uml2/src/hu/elte/txtuml/export/uml2/UML2.java
@@ -1,56 +1,69 @@
 package hu.elte.txtuml.export.uml2;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.IPackageFragment;
-import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 
+import hu.elte.txtuml.eclipseutils.NotFoundException;
+import hu.elte.txtuml.eclipseutils.PackageUtils;
+import hu.elte.txtuml.eclipseutils.ProjectUtils;
 import hu.elte.txtuml.export.uml2.transform.backend.ExportException;
 import hu.elte.txtuml.export.uml2.transform.backend.RuntimeExportException;
 import hu.elte.txtuml.export.uml2.transform.exporters.ModelExporter;
+import hu.elte.txtuml.export.uml2.utils.ModelUtils;
 import hu.elte.txtuml.export.uml2.utils.SharedUtils;
+import hu.elte.txtuml.utils.Sneaky;
 
 /**
  * This class is responsible for exporting Eclipse UML2 model generated from a
  * txtUML model.
- * 
- * @author Adam Ancsin
- *
  */
 public class UML2 {
 
-	public static void exportModel(String sourceProject, String modelName, String outputDirectory) throws Exception {
-		IProject project;
-		project = ResourcesPlugin.getWorkspace().getRoot().getProject(sourceProject);
-		if (project == null || !project.exists()) {
-			throw new Exception("Cannot find project '" + sourceProject + "'");
+	public static void exportModel(String sourceProject, String packageName,
+			String outputDirectory) throws NotFoundException,
+			JavaModelException, IOException, ExportException {
+
+		IJavaProject javaProject = ProjectUtils.findJavaProject(sourceProject);
+
+		IPackageFragment[] packageFragments = PackageUtils
+				.findPackageFragments(javaProject, packageName);
+
+		if (packageFragments.length == 0) {
+			throw new NotFoundException("Cannot find package '" + packageName
+					+ "'");
 		}
 
-		IJavaProject javaProject = JavaCore.create(project);
-		String modelPackageName = modelName.substring(0, modelName.lastIndexOf('.'));
+		exportModel(packageName, packageFragments, javaProject, outputDirectory);
+	}
 
-		List<ICompilationUnit> compUnits = new LinkedList<>();
+	/**
+	 * Obtains the txtUML model from the specified compilation unit.
+	 * 
+	 * @param compilationUnit
+	 *            The specified compilation unit.
+	 * @return The type declaration of the txtUML model.
+	 */
+	private static Optional<String> obtainModelFromCompilationUnits(
+			String packageName, CompilationUnit[] compilationUnits) {
 
-		for (IPackageFragment fragment : javaProject.getPackageFragments()) {
-			if (fragment.getElementName().equals(modelPackageName)) {
-				for (ICompilationUnit compUnit : fragment.getCompilationUnits()) {
-					compUnits.add(compUnit);
-				}
+		Optional<String> ret;
+		
+		for (CompilationUnit cu : compilationUnits) {
+			ret = ModelUtils.findModelNameInTopPackage(cu);
+			if (ret.isPresent()) {
+				return ret;
 			}
 		}
 
-		exportModelElements(modelName, compUnits, javaProject, outputDirectory);
+		return Optional.empty();
 	}
 
 	/**
@@ -59,10 +72,10 @@ public class UML2 {
 	 * @param txtUMLModelName
 	 *            The name of the class representing the txtUML model.
 	 * 
-	 * @param compUnits
+	 * @param sourceFile
 	 *            The Java source file containing the txtUML model.
 	 * 
-	 * @param project
+	 * @param javaProject
 	 *            The Java project contatining the txtUML model.
 	 * 
 	 * @param outputDirectory
@@ -71,21 +84,31 @@ public class UML2 {
 	 * @throws IOException
 	 * @throws JavaModelException
 	 * @throws ExportException
-	 * 
-	 * @author Adam Ancsin
 	 */
-	public static void exportModelElements(String txtUMLModelName, List<ICompilationUnit> compUnits,
-			IJavaProject project, String outputDirectory) throws JavaModelException, IOException, ExportException {
+	public static void exportModel(String packageName,
+			IPackageFragment[] packageFragments, IJavaProject javaProject,
+			String outputDirectory) throws JavaModelException, IOException,
+			ExportException {
 
-		List<CompilationUnit> parsedCompUnits = new LinkedList<>();
+		Stream<ICompilationUnit> packageInfo = Stream.of(packageFragments)
+				.filter(pf -> pf.getElementName().equals(packageName))
+				.map(pf -> pf.getCompilationUnit(PackageUtils.PACKAGE_INFO))
+				.filter(ICompilationUnit::exists);
 
-		for (ICompilationUnit compUnit : compUnits) {
-			IResource resource = compUnit.getResource();
-			File file = new File(resource.getLocationURI());
-			parsedCompUnits.add(SharedUtils.parseJavaSource(file, project));
+		Optional<String> JtxtUMLModelName = obtainModelFromCompilationUnits(
+				packageName, SharedUtils.parseICompilationUnitStream(packageInfo, javaProject));
+
+		if (!JtxtUMLModelName.isPresent()) {
+			throw new ExportException("Package '" + packageName
+					+ "' is not a JtxtUML model.");
 		}
 
-		ModelExporter modelExporter = new ModelExporter(parsedCompUnits, txtUMLModelName, outputDirectory);
+		// Sneaky.<JavaModelException> Throw();
+		Stream<ICompilationUnit> all = Stream.of(packageFragments).flatMap(
+				Sneaky.unchecked(pf -> Stream.of(pf.getCompilationUnits())));
+
+		ModelExporter modelExporter = new ModelExporter(SharedUtils.parseICompilationUnitStream(all,
+				javaProject), JtxtUMLModelName.get(), packageName, outputDirectory);
 		try {
 			modelExporter.exportModel();
 
@@ -96,4 +119,5 @@ public class UML2 {
 		}
 
 	}
+
 }
--- plugins/hu.elte.txtuml.export.uml2/src/hu/elte/txtuml/export/uml2/transform/exporters/ModelExporter.java
+++ plugins/hu.elte.txtuml.export.uml2/src/hu/elte/txtuml/export/uml2/transform/exporters/ModelExporter.java
@@ -1,8 +1,19 @@
 package hu.elte.txtuml.export.uml2.transform.exporters;
 
+import hu.elte.txtuml.export.uml2.mapping.ModelMapCollector;
+import hu.elte.txtuml.export.uml2.mapping.ModelMapException;
+import hu.elte.txtuml.export.uml2.transform.backend.ExportException;
+import hu.elte.txtuml.export.uml2.transform.backend.RuntimeExportException;
+import hu.elte.txtuml.export.uml2.transform.visitors.AssociationVisitor;
+import hu.elte.txtuml.export.uml2.transform.visitors.AttributeVisitor;
+import hu.elte.txtuml.export.uml2.transform.visitors.ClassifierVisitor;
+import hu.elte.txtuml.export.uml2.transform.visitors.MethodSkeletonVisitor;
+import hu.elte.txtuml.export.uml2.utils.ElementTypeTeller;
+import hu.elte.txtuml.export.uml2.utils.ResourceSetFactory;
+
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
@@ -25,32 +36,18 @@ import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.UMLPackage;
 import org.eclipse.uml2.uml.resource.UMLResource;
 
-import hu.elte.txtuml.export.uml2.mapping.ModelMapCollector;
-import hu.elte.txtuml.export.uml2.mapping.ModelMapException;
-import hu.elte.txtuml.export.uml2.transform.backend.ExportException;
-import hu.elte.txtuml.export.uml2.transform.backend.RuntimeExportException;
-import hu.elte.txtuml.export.uml2.transform.visitors.AssociationVisitor;
-import hu.elte.txtuml.export.uml2.transform.visitors.AttributeVisitor;
-import hu.elte.txtuml.export.uml2.transform.visitors.ClassifierVisitor;
-import hu.elte.txtuml.export.uml2.transform.visitors.MethodSkeletonVisitor;
-import hu.elte.txtuml.export.uml2.utils.ElementTypeTeller;
-import hu.elte.txtuml.export.uml2.utils.ResourceSetFactory;
-
 /**
  * This class is responsible for generating Eclipse UML2 model from a txtUML
  * model.
- *
- * @author Adam Ancsin
- *
  */
 public class ModelExporter {
 
 	private static final String STDLIB_URI = "pathmap://TXTUML_STDLIB/stdlib.uml";
 
-	private final String txtUMLModelName;
+	private final String sourcePackageName;
 	private final String outputDirectory;
 	private final Model exportedModel;
-	private final List<CompilationUnit> sourceModel;
+	private final CompilationUnit[] compilationUnits;
 	private final ResourceSet resourceSet;
 	private final Resource modelResource;
 	private final TypeExporter typeExporter;
@@ -60,20 +57,22 @@ public class ModelExporter {
 	private Map<TypeDeclaration, Classifier> classifiers;
 	private Map<TypeDeclaration, Map<MethodDeclaration, Operation>> methods;
 
-	public ModelExporter(List<CompilationUnit> sourceModel, String txtUMLModelName, String outputDirectory)
-			throws ExportException {
-		this.sourceModel = sourceModel;
-		this.txtUMLModelName = txtUMLModelName;
+	public ModelExporter(CompilationUnit[] compilationUnits,
+			String JtxtUMLModelName, String sourcePackageName,
+			String outputDirectory) throws ExportException {
+		this.compilationUnits = compilationUnits;
+		this.sourcePackageName = sourcePackageName;
 		this.outputDirectory = outputDirectory;
 
 		this.exportedModel = UMLFactory.eINSTANCE.createModel();
-		this.exportedModel.setName(txtUMLModelName);
+		this.exportedModel.setName(JtxtUMLModelName);
 
 		this.resourceSet = new ResourceSetFactory().createAndInitResourceSet();
-		this.modelResource = createAndInitModelResource(txtUMLModelName, outputDirectory, resourceSet, exportedModel);
+		this.modelResource = createAndInitModelResource(sourcePackageName,
+				outputDirectory, resourceSet, exportedModel);
 		this.mapping = new ModelMapCollector(modelResource.getURI());
 
-		this.importStandardLibrary(resourceSet, exportedModel);
+		importStandardLibrary(resourceSet, exportedModel);
 
 		this.typeExporter = new TypeExporter(this);
 		this.regionExporter = new RegionExporter(this);
@@ -90,19 +89,23 @@ public class ModelExporter {
 	 * 
 	 * @see hu.elte.txtuml.stdlib
 	 */
-	private void importStandardLibrary(ResourceSet resourceSet, Model exportedModel) {
+	private static void importStandardLibrary(ResourceSet resourceSet,
+			Model exportedModel) {
 		// Load standard library
-		Resource resource = resourceSet.getResource(URI.createURI(STDLIB_URI), true);
+		Resource resource = resourceSet.getResource(URI.createURI(STDLIB_URI),
+				true);
 		if (resource == null) {
 			return;
 		}
-		Package stdLib = (Package) EcoreUtil.getObjectByType(resource.getContents(), UMLPackage.Literals.PACKAGE);
+		Package stdLib = (Package) EcoreUtil.getObjectByType(
+				resource.getContents(), UMLPackage.Literals.PACKAGE);
 		if (stdLib == null) {
 			return;
 		}
 
 		// Import standard library into the generated model
-		PackageImport packageImport = UMLFactory.eINSTANCE.createPackageImport();
+		PackageImport packageImport = UMLFactory.eINSTANCE
+				.createPackageImport();
 		packageImport.setImportedPackage(stdLib);
 		exportedModel.getPackageImports().add(packageImport);
 	}
@@ -118,11 +121,9 @@ public class ModelExporter {
 	 *            The resource set to add the new resource to.
 	 * @param exportedModel
 	 *            The EMF-UML2 model.
-	 *
-	 * @author Adam Ancsin
 	 */
-	private static Resource createAndInitModelResource(String txtUMLModelName, String outputPath,
-			ResourceSet resourceSet, Model exportedModel) {
+	private static Resource createAndInitModelResource(String txtUMLModelName,
+			String outputPath, ResourceSet resourceSet, Model exportedModel) {
 		URI uri = URI.createURI(outputPath).appendSegment(txtUMLModelName)
 				.appendFileExtension(UMLResource.FILE_EXTENSION);
 		Resource modelResource = resourceSet.createResource(uri);
@@ -136,8 +137,6 @@ public class ModelExporter {
 	 * @return The exported UML2 model.
 	 * @throws ExportException
 	 * @throws RuntimeExportException
-	 *
-	 * @author Adam Ancsin
 	 */
 	public Model exportModel() throws ExportException, RuntimeExportException {
 
@@ -148,9 +147,10 @@ public class ModelExporter {
 		exportAttributesOfEveryClassifier();
 		exportMethodSkeletonsOfEveryClassifier();
 		exportStateMachinesOfEveryClass();
-		exportMethodBodiesOfEveryClassifier();
+// TODO: Uncomment this	when activity export gets fixed.	
+//		exportMethodBodiesOfEveryClassifier();
 
-		this.mapping.put(txtUMLModelName, exportedModel);
+		this.mapping.put(sourcePackageName, exportedModel);
 		finishModelExport();
 
 		return this.exportedModel;
@@ -159,25 +159,24 @@ public class ModelExporter {
 	/**
 	 * Exports all classifiers (classes and signals) from the source txtUML
 	 * model.
-	 *
-	 * @author Adam Ancsin
 	 */
 	private void exportClassifiers() {
-		ClassifierVisitor visitor = new ClassifierVisitor(new ClassifierExporter(mapping, exportedModel), true);
-		sourceModel.forEach(cl -> cl.accept(visitor));
+		ClassifierVisitor visitor = new ClassifierVisitor(
+				new ClassifierExporter(mapping, exportedModel), true);
+		Stream.of(compilationUnits).forEach(cu -> cu.accept(visitor));
 		classifiers = visitor.getVisitedClassifiers();
 	}
 
 	/**
 	 * Exports all associations from the source txtUML model.
 	 *
-	 * @author Adam Ancsin
 	 * @throws ExportException
-	 * 
 	 */
 	private void exportAssociations() throws ExportException {
 		try {
-			sourceModel.forEach(cl -> cl.accept(new AssociationVisitor(mapping, exportedModel)));
+			Stream.of(compilationUnits).forEach(
+					cu -> cu.accept(new AssociationVisitor(mapping,
+							exportedModel)));
 		} catch (RuntimeExportException e) {
 			throw e.getCause();
 		}
@@ -185,8 +184,6 @@ public class ModelExporter {
 
 	/**
 	 * Exports all generalizations from the source txtUML model.
-	 * 
-	 * @author Adam Ancsin
 	 */
 	private void exportGeneralizations() {
 		for (TypeDeclaration classifierDeclaration : this.classifiers.keySet()) {
@@ -202,56 +199,60 @@ public class ModelExporter {
 	 * 
 	 * @param classifierDeclaration
 	 *            The declaration of the specified subtype classifier.
-	 *
-	 * @author Adam Ancsin
 	 */
 	private void exportGeneralization(TypeDeclaration classifierDeclaration) {
-		ITypeBinding superclassBinding = classifierDeclaration.resolveBinding().getSuperclass();
+		ITypeBinding superclassBinding = classifierDeclaration.resolveBinding()
+				.getSuperclass();
 		final String generalName = superclassBinding.getName();
-		final String specificName = classifierDeclaration.getName().getFullyQualifiedName();
+		final String specificName = classifierDeclaration.getName()
+				.getFullyQualifiedName();
 
-		Classifier specific = (Classifier) exportedModel.getOwnedType(specificName);
-		Classifier general = (Classifier) exportedModel.getOwnedType(generalName);
+		Classifier specific = (Classifier) exportedModel
+				.getOwnedType(specificName);
+		Classifier general = (Classifier) exportedModel
+				.getOwnedType(generalName);
 
 		specific.createGeneralization(general);
 	}
 
 	/**
 	 * Exports the attributes of every classifier in the model.
-	 * 
-	 * @author Adam Ancsin
 	 */
 	private void exportAttributesOfEveryClassifier() {
-		classifiers.entrySet().forEach(entry -> {
-			Classifier classifier = entry.getValue();
-			TypeDeclaration classifierDeclaration = entry.getKey();
-			exportAttributesOfSpecificClassifier(classifier, classifierDeclaration);
-		});
+		classifiers.entrySet().forEach(
+				entry -> {
+					Classifier classifier = entry.getValue();
+					TypeDeclaration classifierDeclaration = entry.getKey();
+					exportAttributesOfSpecificClassifier(classifier,
+							classifierDeclaration);
+				});
 	}
 
-	private void exportAttributesOfSpecificClassifier(Classifier classifier, TypeDeclaration classifierDeclaration) {
-		AttributeVisitor visitor = new AttributeVisitor(new AttributeExporter(typeExporter, mapping, classifier),
-				classifierDeclaration);
+	private void exportAttributesOfSpecificClassifier(Classifier classifier,
+			TypeDeclaration classifierDeclaration) {
+		AttributeVisitor visitor = new AttributeVisitor(new AttributeExporter(
+				typeExporter, mapping, classifier), classifierDeclaration);
 		classifierDeclaration.accept(visitor);
 	}
 
 	/**
 	 * Exports the member function skeletons of every classifier in the model.
-	 * 
-	 * @author Adam Ancsin
 	 */
 	private void exportMethodSkeletonsOfEveryClassifier() {
 		classifiers.forEach((classifierDeclaration, classifier) -> {
 			if (classifier instanceof Class) {
 				Class specifiedClass = (Class) classifier;
-				exportMethodSkeletonsOfSpecificClass(classifierDeclaration, specifiedClass);
+				exportMethodSkeletonsOfSpecificClass(classifierDeclaration,
+						specifiedClass);
 			}
 		});
 	}
 
-	private void exportMethodSkeletonsOfSpecificClass(TypeDeclaration classDeclaration, Class specificClass) {
+	private void exportMethodSkeletonsOfSpecificClass(
+			TypeDeclaration classDeclaration, Class specificClass) {
 		MethodSkeletonVisitor visitor = new MethodSkeletonVisitor(
-				new MethodSkeletonExporter(typeExporter, specificClass), classDeclaration);
+				new MethodSkeletonExporter(typeExporter, specificClass),
+				classDeclaration);
 		classDeclaration.accept(visitor);
 		methods.put(classDeclaration, visitor.getVisitedMethods());
 	}
@@ -264,39 +265,46 @@ public class ModelExporter {
 		});
 	}
 
-	private void exportStateMachine(TypeDeclaration classifierDeclaration, Class ownerClass) {
-		StateMachine stateMachine = (StateMachine) ownerClass.createClassifierBehavior(ownerClass.getName(),
-				UMLPackage.Literals.STATE_MACHINE);
+	private void exportStateMachine(TypeDeclaration classifierDeclaration,
+			Class ownerClass) {
+		StateMachine stateMachine = (StateMachine) ownerClass
+				.createClassifierBehavior(ownerClass.getName(),
+						UMLPackage.Literals.STATE_MACHINE);
 		Region region = stateMachine.createRegion(ownerClass.getName());
-		regionExporter.exportRegion(classifierDeclaration, stateMachine, region);
+		regionExporter
+				.exportRegion(classifierDeclaration, stateMachine, region);
 	}
 
+	@SuppressWarnings("unused")
 	private void exportMethodBodiesOfEveryClassifier() {
 		classifiers.forEach((declaration, classifier) -> {
 			if (classifier instanceof Class) {
-				exportMethodBodiesOfSpecificClass(declaration, (Class) classifier);
+				exportMethodBodiesOfSpecificClass(declaration,
+						(Class) classifier);
 			}
 		});
 	}
 
-	private void exportMethodBodiesOfSpecificClass(TypeDeclaration classDeclaration, Class specificClass) {
-		Map<MethodDeclaration, Operation> memberFunctions = methods.get(classDeclaration);
+	private void exportMethodBodiesOfSpecificClass(
+			TypeDeclaration classDeclaration, Class specificClass) {
+		Map<MethodDeclaration, Operation> memberFunctions = methods
+				.get(classDeclaration);
 		memberFunctions.forEach((methodDeclaration, operation) -> {
 			String methodName = operation.getName();
-			Activity activity = (Activity) specificClass.createOwnedBehavior(methodName, UMLPackage.Literals.ACTIVITY);
+			Activity activity = (Activity) specificClass.createOwnedBehavior(
+					methodName, UMLPackage.Literals.ACTIVITY);
 			activity.setSpecification(operation);
-			MethodBodyExporter.export(activity, this, methodDeclaration, operation.getOwnedParameters());
+			MethodBodyExporter.export(activity, this, methodDeclaration,
+					operation.getOwnedParameters());
 		});
 	}
 
 	/**
 	 * Finishes the model export in progress.
-	 * 
-	 * @author Adam Ancsin
 	 */
 	private void finishModelExport() {
 		try {
-			mapping.save(URI.createURI(outputDirectory), txtUMLModelName);
+			mapping.save(URI.createURI(outputDirectory), sourcePackageName);
 		} catch (ModelMapException e) {
 			System.out.println("Faild to save model mapping.");
 		}
@@ -308,8 +316,6 @@ public class ModelExporter {
 
 	/**
 	 * @return The resource containing the currently exported model.
-	 *
-	 * @author Adam Ancsin
 	 */
 	public Resource getModelResource() {
 		return this.modelResource;
@@ -317,8 +323,6 @@ public class ModelExporter {
 
 	/**
 	 * @return The exported UML2 model.
-	 *
-	 * @author Adam Ancsin
 	 */
 	public Model getExportedModel() {
 		return this.exportedModel;
