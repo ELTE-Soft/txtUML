18fd6fcdfb97c51ad4f4f78eb71cbc44af69018a
Nagy András <nagyandras95@users.noreply.github.com>
2016-02-23 16:39:53 +0100

    Modified main.cpp according to new runtime generation

--- dev/plugins/hu.elte.txtuml.export.cpp/cpp-runtime/main.cpp
+++ dev/plugins/hu.elte.txtuml.export.cpp/cpp-runtime/main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
-
-#include "runtime/runtime.hpp"
-
 #include "deployment.hpp"
+
 int main()
 {
-  //RuntimeI* rt=deployment::createThrededRuntime(); // the runtime type depends on deployment header options
+  //Runtime* rt=deployment::createThreadedRuntime(); // the runtime type depends on deployment header options
 
-  //Class obj(rt); //create the object and emplace the object into the runtime
+  //Class obj(); //create the object
   //obj.startSM(); //starts the statemachine of the object
   //obj.send(EventPtr(new SignalName_EC(obj,Class::SignalName_EE))); //send a signal to the object
 
