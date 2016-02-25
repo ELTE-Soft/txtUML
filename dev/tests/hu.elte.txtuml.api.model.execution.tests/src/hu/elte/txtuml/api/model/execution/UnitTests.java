677ecbebb96ad88ec41060cbc6d88a07d94409f0
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2016-02-25 16:12:59 +0100

    Removed api.stdlib tests from api.model.execution.tests project.

--- dev/tests/hu.elte.txtuml.api.model.execution.tests/src/hu/elte/txtuml/api/model/execution/UnitTests.java
+++ dev/tests/hu.elte.txtuml.api.model.execution.tests/src/hu/elte/txtuml/api/model/execution/UnitTests.java
@@ -9,12 +9,13 @@ import hu.elte.txtuml.api.model.execution.error.deletion.AllDeletionErrorTests;
 import hu.elte.txtuml.api.model.execution.error.multiplicity.AllAssociationEndErrorTests;
 import hu.elte.txtuml.api.model.execution.error.other.AllOtherErrorTests;
 import hu.elte.txtuml.api.model.execution.error.statemachine.AllStateMachineErrorTests;
+import hu.elte.txtuml.api.model.execution.shutdown.ShutdownWithTenMessagesTest;
 import hu.elte.txtuml.api.model.execution.statemachine.AllStateMachineTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AllPackagePrivateTests.class, AllAssociationEndTests.class,
 		AllStateMachineTests.class, AllDeletionErrorTests.class,
 		AllStateMachineErrorTests.class, AllAssociationEndErrorTests.class,
-		AllOtherErrorTests.class })
+		AllOtherErrorTests.class, ShutdownWithTenMessagesTest.class })
 public class UnitTests {
 }
--- dev/tests/hu.elte.txtuml.api.model.execution.tests/src/hu/elte/txtuml/api/model/execution/error/other/AllOtherErrorTests.java
+++ dev/tests/hu.elte.txtuml.api.model.execution.tests/src/hu/elte/txtuml/api/model/execution/error/other/AllOtherErrorTests.java
@@ -5,8 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ ChangingLockedExecutionTimeMultiplierTest.class,
-		UnlinkingNonExistingAssociationTest.class,
+@SuiteClasses({ UnlinkingNonExistingAssociationTest.class,
 		ModelObjectCreationFailureTest.class })
 public class AllOtherErrorTests {
 }
