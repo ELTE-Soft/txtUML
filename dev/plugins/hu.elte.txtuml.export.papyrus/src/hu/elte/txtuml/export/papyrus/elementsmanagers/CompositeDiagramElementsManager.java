03af4ed46159df269f1a6779a2edf8bc877022e2
Gergely Dévai <deva@elte.hu>
2016-03-01 16:28:32 +0100

    Generating empty composite structure diagrams - adding new source files

---
+++ dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/elementsmanagers/CompositeDiagramElementsManager.java
@@ -0,0 +1,19 @@
+package hu.elte.txtuml.export.papyrus.elementsmanagers;
+
+import java.util.List;
+
+import org.eclipse.gmf.runtime.diagram.ui.editparts.DiagramEditPart;
+import org.eclipse.uml2.uml.Element;
+
+public class CompositeDiagramElementsManager extends AbstractDiagramElementsManager {
+
+	public CompositeDiagramElementsManager(DiagramEditPart diagramEditPart) {
+		super(diagramEditPart);
+	}
+
+	@Override
+	public void addElementsToDiagram(List<Element> elements) {
+		// TODO
+	}
+
+}
