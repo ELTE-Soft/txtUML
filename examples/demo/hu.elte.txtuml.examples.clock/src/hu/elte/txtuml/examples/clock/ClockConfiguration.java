dca9f2b8513c2a842072f7b8370827c916c353fc
Gergely Dévai <deva@elte.hu>
2016-03-03 22:33:03 +0100

    Fix an import.

--- examples/demo/hu.elte.txtuml.examples.clock/src/hu/elte/txtuml/examples/clock/ClockConfiguration.java
+++ examples/demo/hu.elte.txtuml.examples.clock/src/hu/elte/txtuml/examples/clock/ClockConfiguration.java
@@ -2,7 +2,6 @@ package hu.elte.txtuml.examples.clock;
 
 import hu.elte.txtuml.api.deployment.Configuration;
 import hu.elte.txtuml.api.deployment.Group;
-import hu.elte.txtuml.api.deployment.Multithreading;
 import hu.elte.txtuml.examples.clock.model.classes.Display;
 import hu.elte.txtuml.examples.clock.model.classes.Hand;
 import hu.elte.txtuml.examples.clock.model.classes.Pendulum;
