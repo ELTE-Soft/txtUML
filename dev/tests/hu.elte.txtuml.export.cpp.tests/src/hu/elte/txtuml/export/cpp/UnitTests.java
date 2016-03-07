c8452945402feab3f3e62cc829a121452194f6b1
Zoltán Gera <gerazo80@gmail.com>
2016-03-07 11:21:04 +0100

    Add test project for export.cpp

---
+++ dev/tests/hu.elte.txtuml.export.cpp.tests/src/hu/elte/txtuml/export/cpp/UnitTests.java
@@ -0,0 +1,13 @@
+package hu.elte.txtuml.export.cpp;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	})
+public class UnitTests {
+
+}
+
