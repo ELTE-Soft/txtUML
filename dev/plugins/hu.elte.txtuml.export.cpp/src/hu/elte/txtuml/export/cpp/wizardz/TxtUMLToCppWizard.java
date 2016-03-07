7d5c3825185deed884d4641874ce0eaa729de164
Zoltán Gera <gerazo80@gmail.com>
2016-03-07 15:48:07 +0100

    CPP wizard is testable

--- dev/plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/wizardz/TxtUMLToCppWizard.java
+++ dev/plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/wizardz/TxtUMLToCppWizard.java
@@ -13,7 +13,6 @@ import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.uml2.uml.Model;
 import org.eclipse.uml2.uml.UMLPackage;
 
-
 import hu.elte.txtuml.export.uml2.UML2.ExportMode;
 import hu.elte.txtuml.api.deployment.Configuration;
 import hu.elte.txtuml.export.ExportUtils;
@@ -26,15 +25,15 @@ import hu.elte.txtuml.utils.eclipse.Dialogs;
 
 public class TxtUMLToCppWizard extends Wizard {
 
-	private static final String GenericFolderName = "cpp-gen";
+	private static final String GeneratedCPPFolderName = "cpp-gen";
 	private static final String UmlFilesFolderName = "model";
 
+	private boolean testing = false;
 	private TxtUMLToCppPage createCppCodePage;
 
 	public TxtUMLToCppWizard() {
 		super();
 		setNeedsProgressMonitor(true);
-
 	}
 
 	@Override
@@ -48,12 +47,9 @@ public class TxtUMLToCppWizard extends Wizard {
 		addPage(createCppCodePage);
 	}
 
-	@SuppressWarnings("unchecked")
 	@Override
 	public boolean performFinish() {
-
 		try {
-
 			String txtUMLProject = createCppCodePage.getProject();
 			String txtUMLModel = createCppCodePage.getModel();
 			String threadManagmentDescription = createCppCodePage.getThreadDescription();
@@ -64,77 +60,87 @@ public class TxtUMLToCppWizard extends Wizard {
 
 			boolean addRuntimeOption = createCppCodePage.getAddRuntimeOptionSelection();
 
-			String projectFolder = ResourcesPlugin.getWorkspace().getRoot().getProject(txtUMLProject).getLocation()
-					.toFile().getAbsolutePath();
-
-			String umlFilesFolder = txtUMLProject + File.separator + GenericFolderName + File.separator + txtUMLModel
-					+ File.separator + UmlFilesFolderName;
-			String umlFileLocation = umlFilesFolder + File.separator + txtUMLModel + ".uml";
-
-			try {
-				ExportUtils.exportTxtUMLModelToUML2(txtUMLProject, txtUMLModel, umlFilesFolder,
-						ExportMode.ExportActionCode);
-			} catch (Exception e) {
-				Dialogs.errorMsgb("txtUML export Error", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
-						e);
-				return false;
-			}
-
-			Model model = Uml2Utils.loadModel(URI.createPlatformResourceURI(umlFileLocation, false));
-
-			URLClassLoader loader = ClassLoaderProvider.getClassLoaderForProject(txtUMLProject,
-					ThreadDescriptionExporter.class.getClassLoader());
-			Class<?> txtUMLThreadDescription;
-			try {
-				 txtUMLThreadDescription = loader.loadClass(threadManagmentDescription);
-			}
-			catch(ClassNotFoundException e) {
-				Dialogs.errorMsgb("Description Class Error", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
-						e);
-				return false;
-			}
-			
-			
-			List<org.eclipse.uml2.uml.Class> classList = new ArrayList<org.eclipse.uml2.uml.Class>();
-			Shared.getTypedElements(classList, model.getOwnedElements(), UMLPackage.Literals.CLASS);
-			Set<String> allClass = new HashSet<String>();
-			for(org.eclipse.uml2.uml.Class cls: classList) {
-				allClass.add(cls.getName());
-			}
-			
-			ThreadDescriptionExporter exporter = new ThreadDescriptionExporter(allClass);
-			exporter.exportDescription((Class<? extends Configuration>) txtUMLThreadDescription);
-
-			if (!exporter.warningListIsEmpty()) {
-				String warnings = "";
-				for (String warning : exporter.getWarnings()) {
-					warnings += warning + "\n";
-				}
-
-				warnings += "\nWould you like to continue the generation?\n ";
-				if (!Dialogs.WarningConfirm("Description export warnings", warnings)) {
-					return false;
-				}
-			}
-
-			Uml2ToCppExporter cppExporter = new Uml2ToCppExporter(model, exporter.getConfigMap(), addRuntimeOption);
-			try {
-				cppExporter.buildCppCode(
-						projectFolder + File.separator + GenericFolderName + File.separator + txtUMLModel);
-
-			} catch (Exception e) {
-				Dialogs.errorMsgb("Compilation failed",e.getClass() + ":" + System.lineSeparator() + e.getMessage(), e);
-
-			}
-
+			uml2ToCpp(txtUMLProject, txtUMLModel, threadManagmentDescription, addRuntimeOption);
 		} catch (Exception e) {
-
 			e.printStackTrace();
 			return false;
 		}
 
 		return true;
-
 	}
 
+	@SuppressWarnings("unchecked")
+	void uml2ToCpp(String txtUMLProject, String txtUMLModel, String threadManagmentDescription,
+			boolean addRuntimeOption) throws Exception {
+		String projectFolder = ResourcesPlugin.getWorkspace().getRoot().getProject(txtUMLProject).getLocation().toFile()
+				.getAbsolutePath();
+		String umlFilesFolder = txtUMLProject + File.separator + GeneratedCPPFolderName + File.separator + txtUMLModel
+				+ File.separator + UmlFilesFolderName;
+		String umlFileLocation = umlFilesFolder + File.separator + txtUMLModel + ".uml";
+
+		try {
+			ExportUtils.exportTxtUMLModelToUML2(txtUMLProject, txtUMLModel, umlFilesFolder,
+					ExportMode.ExportActionCode);
+		} catch (Exception e) {
+			if (!testing) {
+				Dialogs.errorMsgb("txtUML export Error", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
+						e);
+			}
+			throw e;
+		}
+
+		Model model = Uml2Utils.loadModel(URI.createPlatformResourceURI(umlFileLocation, false));
+
+		URLClassLoader loader = ClassLoaderProvider.getClassLoaderForProject(txtUMLProject,
+				ThreadDescriptionExporter.class.getClassLoader());
+		Class<? extends Configuration> txtUMLThreadDescription;
+		try {
+			txtUMLThreadDescription = (Class<? extends Configuration>) loader.loadClass(threadManagmentDescription);
+		} catch (ClassNotFoundException e) {
+			if (!testing) {
+				Dialogs.errorMsgb("Description Class Error",
+						e.getClass() + ":" + System.lineSeparator() + e.getMessage(), e);
+			}
+			throw e;
+		}
+
+		List<org.eclipse.uml2.uml.Class> classList = new ArrayList<org.eclipse.uml2.uml.Class>();
+		Shared.getTypedElements(classList, model.getOwnedElements(), UMLPackage.Literals.CLASS);
+		Set<String> allClass = new HashSet<String>();
+		for (org.eclipse.uml2.uml.Class cls : classList) {
+			allClass.add(cls.getName());
+		}
+
+		ThreadDescriptionExporter exporter = new ThreadDescriptionExporter(allClass);
+		exporter.exportDescription(txtUMLThreadDescription);
+
+		if (!exporter.warningListIsEmpty()) {
+			StringBuilder warnings = new StringBuilder("");
+			for (String warning : exporter.getWarnings()) {
+				warnings.append(warning + "\n");
+			}
+			warnings.append("\nWould you like to continue the generation?\n ");
+			if (testing || !Dialogs.WarningConfirm("Description export reported warnings", warnings.toString())) {
+				throw new Exception("Export was cancelled by user because of warnings");
+			}
+		}
+
+		Uml2ToCppExporter cppExporter = new Uml2ToCppExporter(model, exporter.getConfigMap(), addRuntimeOption);
+		try {
+			cppExporter.buildCppCode(
+					projectFolder + File.separator + GeneratedCPPFolderName + File.separator + txtUMLModel);
+		} catch (Exception e) {
+			if (!testing) {
+				Dialogs.errorMsgb("Compilation failed", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
+						e);
+			}
+			throw e;
+		}
+	}
+
+	// for testing
+	TxtUMLToCppWizard(boolean testing) {
+		super();
+		this.testing = true;
+	}
 }
