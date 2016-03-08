dfb6a2101adad457eb4978cf8d1f8c0971b905d5
Zoltán Gera <gerazo80@gmail.com>
2016-03-08 16:38:34 +0100

    Do not create a wizard when testing

--- dev/plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/wizardz/TxtUMLToCppWizard.java
+++ dev/plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/wizardz/TxtUMLToCppWizard.java
@@ -1,34 +1,9 @@
 package hu.elte.txtuml.export.cpp.wizardz;
 
-import java.io.File;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.uml2.uml.Model;
-import org.eclipse.uml2.uml.UMLPackage;
-
-import hu.elte.txtuml.export.uml2.UML2.ExportMode;
-import hu.elte.txtuml.api.deployment.Configuration;
-import hu.elte.txtuml.export.ExportUtils;
-import hu.elte.txtuml.export.Uml2Utils;
-import hu.elte.txtuml.export.cpp.Shared;
-import hu.elte.txtuml.export.cpp.Uml2ToCppExporter;
-import hu.elte.txtuml.export.cpp.thread.ThreadDescriptionExporter;
-import hu.elte.txtuml.utils.eclipse.ClassLoaderProvider;
-import hu.elte.txtuml.utils.eclipse.Dialogs;
 
 public class TxtUMLToCppWizard extends Wizard {
 
-	private static final String GeneratedCPPFolderName = "cpp-gen";
-	private static final String UmlFilesFolderName = "model";
-
-	private boolean testing = false;
 	private TxtUMLToCppPage createCppCodePage;
 
 	public TxtUMLToCppWizard() {
@@ -60,7 +35,8 @@ public class TxtUMLToCppWizard extends Wizard {
 
 			boolean addRuntimeOption = createCppCodePage.getAddRuntimeOptionSelection();
 
-			uml2ToCpp(txtUMLProject, txtUMLModel, threadManagmentDescription, addRuntimeOption);
+			TxtUMLToCppGovernor governor = new TxtUMLToCppGovernor(false);
+			governor.uml2ToCpp(txtUMLProject, txtUMLModel, threadManagmentDescription, addRuntimeOption);
 		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
@@ -69,78 +45,4 @@ public class TxtUMLToCppWizard extends Wizard {
 		return true;
 	}
 
-	@SuppressWarnings("unchecked")
-	void uml2ToCpp(String txtUMLProject, String txtUMLModel, String threadManagmentDescription,
-			boolean addRuntimeOption) throws Exception {
-		String projectFolder = ResourcesPlugin.getWorkspace().getRoot().getProject(txtUMLProject).getLocation().toFile()
-				.getAbsolutePath();
-		String umlFilesFolder = txtUMLProject + File.separator + GeneratedCPPFolderName + File.separator + txtUMLModel
-				+ File.separator + UmlFilesFolderName;
-		String umlFileLocation = umlFilesFolder + File.separator + txtUMLModel + ".uml";
-
-		try {
-			ExportUtils.exportTxtUMLModelToUML2(txtUMLProject, txtUMLModel, umlFilesFolder,
-					ExportMode.ExportActionCode);
-		} catch (Exception e) {
-			if (!testing) {
-				Dialogs.errorMsgb("txtUML export Error", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
-						e);
-			}
-			throw e;
-		}
-
-		Model model = Uml2Utils.loadModel(URI.createPlatformResourceURI(umlFileLocation, false));
-
-		URLClassLoader loader = ClassLoaderProvider.getClassLoaderForProject(txtUMLProject,
-				ThreadDescriptionExporter.class.getClassLoader());
-		Class<? extends Configuration> txtUMLThreadDescription;
-		try {
-			txtUMLThreadDescription = (Class<? extends Configuration>) loader.loadClass(threadManagmentDescription);
-		} catch (ClassNotFoundException e) {
-			if (!testing) {
-				Dialogs.errorMsgb("Description Class Error",
-						e.getClass() + ":" + System.lineSeparator() + e.getMessage(), e);
-			}
-			throw e;
-		}
-
-		List<org.eclipse.uml2.uml.Class> classList = new ArrayList<org.eclipse.uml2.uml.Class>();
-		Shared.getTypedElements(classList, model.getOwnedElements(), UMLPackage.Literals.CLASS);
-		Set<String> allClass = new HashSet<String>();
-		for (org.eclipse.uml2.uml.Class cls : classList) {
-			allClass.add(cls.getName());
-		}
-
-		ThreadDescriptionExporter exporter = new ThreadDescriptionExporter(allClass);
-		exporter.exportDescription(txtUMLThreadDescription);
-
-		if (!exporter.warningListIsEmpty()) {
-			StringBuilder warnings = new StringBuilder("");
-			for (String warning : exporter.getWarnings()) {
-				warnings.append(warning + "\n");
-			}
-			warnings.append("\nWould you like to continue the generation?\n ");
-			if (testing || !Dialogs.WarningConfirm("Description export reported warnings", warnings.toString())) {
-				throw new Exception("Export was cancelled by user because of warnings");
-			}
-		}
-
-		Uml2ToCppExporter cppExporter = new Uml2ToCppExporter(model, exporter.getConfigMap(), addRuntimeOption);
-		try {
-			cppExporter.buildCppCode(
-					projectFolder + File.separator + GeneratedCPPFolderName + File.separator + txtUMLModel);
-		} catch (Exception e) {
-			if (!testing) {
-				Dialogs.errorMsgb("Compilation failed", e.getClass() + ":" + System.lineSeparator() + e.getMessage(),
-						e);
-			}
-			throw e;
-		}
-	}
-
-	// for testing
-	TxtUMLToCppWizard(boolean testing) {
-		super();
-		this.testing = true;
-	}
 }
--- dev/tests/hu.elte.txtuml.export.cpp.tests/src/hu/elte/txtuml/export/cpp/wizardz/CompileTests.java
+++ dev/tests/hu.elte.txtuml.export.cpp.tests/src/hu/elte/txtuml/export/cpp/wizardz/CompileTests.java
@@ -6,29 +6,33 @@ import static org.junit.Assert.assertThat;
 import org.junit.Test;
 
 public class CompileTests {
-	
+
 	private static class Config {
 		final String project;
 		final String model;
 		final String deployment;
+
 		Config(String project, String model, String deployment) {
 			this.project = project;
 			this.model = model;
 			this.deployment = deployment;
 		}
 	}
-	
+
 	private static final Config[] testProjects = {
-			new Config("hu.elte.txtuml.examples.machine", "hu.elte.txtuml.examples.machine.model1", "hu.elte.txtuml.examples.machine.Machine1Configuration"),
-			new Config("hu.elte.txtuml.examples.monitoring", "hu.elte.txtuml.examples.monitoring.model", "hu.elte.txtuml.examples.monitoring.MonitoringConfiguration"),
-			new Config("hu.elte.txtuml.examples.producer_consumer", "hu.elte.txtuml.examples.producer_consumer.model", "hu.elte.txtuml.examples.producer_consumer.ProducerConsumerConfiguration"),
-			new Config("hu.elte.txtuml.examples.train", "hu.elte.txtuml.examples.train.model", "hu.elte.txtuml.examples.train.TrainConfiguration"),
-	};
-	
+			new Config("hu.elte.txtuml.examples.machine", "hu.elte.txtuml.examples.machine.model1",
+					"hu.elte.txtuml.examples.machine.Machine1Configuration"),
+			new Config("hu.elte.txtuml.examples.monitoring", "hu.elte.txtuml.examples.monitoring.model",
+					"hu.elte.txtuml.examples.monitoring.MonitoringConfiguration"),
+			new Config("hu.elte.txtuml.examples.producer_consumer", "hu.elte.txtuml.examples.producer_consumer.model",
+					"hu.elte.txtuml.examples.producer_consumer.ProducerConsumerConfiguration"),
+			new Config("hu.elte.txtuml.examples.train", "hu.elte.txtuml.examples.train.model",
+					"hu.elte.txtuml.examples.train.TrainConfiguration"), };
+
 	@Test
 	public void exportTest() {
 		for (Config config : testProjects) {
-			TxtUMLToCppWizard cppgen = new TxtUMLToCppWizard(true);
+			TxtUMLToCppGovernor cppgen = new TxtUMLToCppGovernor(true);
 			boolean runsOk = true;
 			try {
 				cppgen.uml2ToCpp(config.project, config.model, config.deployment, false);
@@ -40,4 +44,3 @@ public class CompileTests {
 		}
 	}
 }
-
