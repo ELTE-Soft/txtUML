c4fecd0d66132a7bfd6d1ef1c388038ee1716eb6
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2016-04-12 13:55:20 +0200

    Refactored the pingpong demo.

---
+++ examples/demo/pingpong/src/pingpong/j/model/Game.java
@@ -0,0 +1,53 @@
+package pingpong.j.model;
+
+import hu.elte.txtuml.api.model.Action;
+import hu.elte.txtuml.api.model.From;
+import hu.elte.txtuml.api.model.ModelClass;
+import hu.elte.txtuml.api.model.To;
+import hu.elte.txtuml.api.model.Trigger;
+import pingpong.j.model.Player.BallAtPlayerPort;
+import pingpong.j.model.associations.LeftPlayer;
+import pingpong.j.model.associations.RightPlayer;
+import pingpong.j.model.connectors.Table;
+import pingpong.j.model.signals.Ball;
+
+public class Game extends ModelClass {
+
+	public Game() {
+		Player left = Action.create(Player.class, "Left ");
+		Player right = Action.create(Player.class, "Right");
+
+		Action.link(LeftPlayer.inGame.class, this, LeftPlayer.player.class, left);
+		Action.link(RightPlayer.inGame.class, this, RightPlayer.player.class, right);
+
+		Action.connect(Table.left.class, left.port(BallAtPlayerPort.class), Table.right.class,
+				right.port(BallAtPlayerPort.class));
+
+		Action.start(left);
+		Action.start(right);
+	}
+
+	class Init extends Initial {
+	}
+
+	class Waiting extends State {
+	}
+
+	@From(Init.class)
+	@To(Waiting.class)
+	class Initialize extends Transition {
+	}
+
+	@From(Waiting.class)
+	@To(Waiting.class)
+	@Trigger(Ball.class)
+	class StartGame extends Transition {
+
+		@Override
+		public void effect() {
+			Player left = assoc(LeftPlayer.player.class).selectAny();
+			Action.send(getSignal(), left);
+		}
+
+	}
+}
---
+++ examples/demo/pingpong/src/pingpong/j/model/signals/MissedTheBall.java
@@ -0,0 +1,7 @@
+package pingpong.j.model.signals;
+
+import hu.elte.txtuml.api.model.Signal;
+
+public class MissedTheBall extends Signal {
+
+}
