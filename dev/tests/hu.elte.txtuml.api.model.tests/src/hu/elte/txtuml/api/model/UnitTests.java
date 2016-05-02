4b4d07c349068ad7b988accd78d1a843030e71a6
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2016-05-02 14:03:54 +0200

    Added ShutdownWithTenMessagesTest as a unit test in the api.model.tests project.

--- dev/tests/hu.elte.txtuml.api.model.tests/src/hu/elte/txtuml/api/model/UnitTests.java
+++ dev/tests/hu.elte.txtuml.api.model.tests/src/hu/elte/txtuml/api/model/UnitTests.java
@@ -9,12 +9,13 @@ import hu.elte.txtuml.api.model.error.deletion.AllDeletionErrorTests;
 import hu.elte.txtuml.api.model.error.multiplicity.AllAssociationEndErrorTests;
 import hu.elte.txtuml.api.model.error.other.AllOtherErrorTests;
 import hu.elte.txtuml.api.model.error.statemachine.AllStateMachineErrorTests;
+import hu.elte.txtuml.api.model.shutdown.ShutdownWithTenMessagesTest;
 import hu.elte.txtuml.api.model.statemachine.AllStateMachineTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AllPackagePrivateTests.class, AllAssociationEndTests.class,
 		AllStateMachineTests.class, AllDeletionErrorTests.class,
 		AllStateMachineErrorTests.class, AllAssociationEndErrorTests.class,
-		AllOtherErrorTests.class })
+		AllOtherErrorTests.class, ShutdownWithTenMessagesTest.class })
 public class UnitTests {
 }
