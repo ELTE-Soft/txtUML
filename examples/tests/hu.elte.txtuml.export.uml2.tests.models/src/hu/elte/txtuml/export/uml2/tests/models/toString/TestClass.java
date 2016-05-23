e55f7410f334b27ad19b743b99cf8f5abf4d2ed7
nboldi <nboldi@elte.hu>
2016-05-23 10:16:27 +0200

    Fixed stdlib

--- examples/tests/hu.elte.txtuml.export.uml2.tests.models/src/hu/elte/txtuml/export/uml2/tests/models/toString/TestClass.java
+++ examples/tests/hu.elte.txtuml.export.uml2.tests.models/src/hu/elte/txtuml/export/uml2/tests/models/toString/TestClass.java
@@ -4,13 +4,13 @@ import hu.elte.txtuml.api.model.ModelClass;
 
 public class TestClass extends ModelClass {
 	
-//	public void testToString(TestClass sut) {
-//		String a = sut.toString();
-//	}
-//	
-//	public void testPrimitiveToString() {
-//		String a = Integer.toString(3);
-//	}
+	public void testToString(TestClass sut) {
+		String a = sut.toString();
+	}
+	
+	public void testPrimitiveToString() {
+		String a = Integer.toString(3);
+	}
 	
 	public void testAuto() {
 		String a = "a" + 3;
