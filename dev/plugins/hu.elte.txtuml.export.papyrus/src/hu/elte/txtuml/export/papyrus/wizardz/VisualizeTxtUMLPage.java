622b925cc9a184bf29f6c49301d4427e8f5a7097
Dávid J. Németh <djnemeth@outlook.com>
2016-07-25 08:34:28 +0200

    Auto-format *VisualizeTxtUMLPage.java*

--- dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/wizardz/VisualizeTxtUMLPage.java
+++ dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/wizardz/VisualizeTxtUMLPage.java
@@ -48,209 +48,204 @@ import hu.elte.txtuml.utils.eclipse.ProjectUtils;
  */
 @SuppressWarnings("restriction")
 public class VisualizeTxtUMLPage extends WizardPage {
-	
+
 	private static final String browseButtonText = "Browse...";
-	
+
 	private Composite container;
 	private Text txtUMLModel;
-	//private List<Text> txtUMLLayout = new LinkedList<>();
+	// private List<Text> txtUMLLayout = new LinkedList<>();
 	private Text txtUMLProject;
 	private ScrolledComposite sc;
-	
+
 	private Map<Integer, Text> txtUMLLayout = new HashMap<>();
-	
+
 	/**
 	 * The Constructor
 	 */
 	public VisualizeTxtUMLPage() {
 		super("Visualize txtUML Page");
 		setTitle("Visualize txtUML page");
-	    setDescription("Provide the txtUML project, model package and diagrams to be visualized");
+		setDescription("Provide the txtUML project, model package and diagrams to be visualized");
 	}
-	
+
 	/*
 	 * (non-Javadoc)
-	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
+	 * 
+	 * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.
+	 * widgets.Composite)
 	 */
 	@Override
 	public void createControl(Composite parent) {
 		sc = new ScrolledComposite(parent, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
 		container = new Composite(sc, SWT.NONE);
-		
-		GridLayout layout = new GridLayout(4,false);
-	    container.setLayout(layout);
-	    
-	    
-	    Label label1 = new Label(container, SWT.NONE);
-	    label1.setText("txtUML Project: ");
-	    txtUMLProject = new Text(container, SWT.BORDER | SWT.SINGLE);
-	    txtUMLProject.setText(PreferencesManager.getString(PreferencesManager.TXTUML_VISUALIZE_TXTUML_PROJECT));
-	    Button browseProject = new Button(container, SWT.NONE);
-	    browseProject.setText(browseButtonText);
-	    browseProject.addSelectionListener(new  SelectionListener() {
+
+		GridLayout layout = new GridLayout(4, false);
+		container.setLayout(layout);
+
+		Label label1 = new Label(container, SWT.NONE);
+		label1.setText("txtUML Project: ");
+		txtUMLProject = new Text(container, SWT.BORDER | SWT.SINGLE);
+		txtUMLProject.setText(PreferencesManager.getString(PreferencesManager.TXTUML_VISUALIZE_TXTUML_PROJECT));
+		Button browseProject = new Button(container, SWT.NONE);
+		browseProject.setText(browseButtonText);
+		browseProject.addSelectionListener(new SelectionListener() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				ElementTreeSelectionDialog   dialog =
-						new ElementTreeSelectionDialog(
-								container.getShell(),
-							    new WorkbenchLabelProvider(),
-							    new WorkbenchContentProvider(){
-									@Override
-									public Object[] getChildren(Object element) {
-										if(element instanceof IWorkspaceRoot){
-											return ((IWorkspaceRoot) element).getProjects();
-										}
-										return new Object[0];
-									}
-								});
+				ElementTreeSelectionDialog dialog = new ElementTreeSelectionDialog(container.getShell(),
+						new WorkbenchLabelProvider(), new WorkbenchContentProvider() {
+					@Override
+					public Object[] getChildren(Object element) {
+						if (element instanceof IWorkspaceRoot) {
+							return ((IWorkspaceRoot) element).getProjects();
+						}
+						return new Object[0];
+					}
+				});
 				dialog.setInput(ResourcesPlugin.getWorkspace().getRoot());
 				dialog.setTitle("Project Selection");
 				dialog.open();
 				Object[] result = dialog.getResult();
-				if(result != null && result.length > 0){
+				if (result != null && result.length > 0) {
 					txtUMLProject.setText(((IProject) result[0]).getName());
 				}
 			}
-			
+
 			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
-	    
-	    
-	
-	    Label label2 = new Label(container, SWT.NONE);
-	    label2.setText("txtUML Model: ");
-	    txtUMLModel = new Text(container, SWT.BORDER | SWT.SINGLE);
-	    txtUMLModel.setText(PreferencesManager.getString(PreferencesManager.TXTUML_VISUALIZE_TXTUML_MODEL));
-	    Button browseModel = new Button(container, SWT.NONE);
-	    browseModel.setText(browseButtonText );
-	    browseModel.addSelectionListener(new  SelectionListener() {
-			
+
+		Label label2 = new Label(container, SWT.NONE);
+		label2.setText("txtUML Model: ");
+		txtUMLModel = new Text(container, SWT.BORDER | SWT.SINGLE);
+		txtUMLModel.setText(PreferencesManager.getString(PreferencesManager.TXTUML_VISUALIZE_TXTUML_MODEL));
+		Button browseModel = new Button(container, SWT.NONE);
+		browseModel.setText(browseButtonText);
+		browseModel.addSelectionListener(new SelectionListener() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				JavaSearchScope scope = new JavaSearchScope();
 				try {
-					IJavaProject javaProject = ProjectUtils
-							.findJavaProject(txtUMLProject.getText());
-					for (IPackageFragmentRoot root : PackageUtils
-							.getPackageFragmentRoots(javaProject)) {
+					IJavaProject javaProject = ProjectUtils.findJavaProject(txtUMLProject.getText());
+					for (IPackageFragmentRoot root : PackageUtils.getPackageFragmentRoots(javaProject)) {
 						scope.add(root);
 					}
 				} catch (JavaModelException | NotFoundException ex) {
 				}
-				PackageSelectionDialog dialog = new PackageSelectionDialog(
-						getShell(), getContainer(),
-						PackageSelectionDialog.F_HIDE_DEFAULT_PACKAGE
-								| PackageSelectionDialog.F_REMOVE_DUPLICATES,
+				PackageSelectionDialog dialog = new PackageSelectionDialog(getShell(), getContainer(),
+						PackageSelectionDialog.F_HIDE_DEFAULT_PACKAGE | PackageSelectionDialog.F_REMOVE_DUPLICATES,
 						scope);
 
 				dialog.setTitle("Project Selection");
 				dialog.open();
 				Object[] result = dialog.getResult();
 				if (result != null && result.length > 0) {
-					txtUMLModel.setText(((IPackageFragment) result[0])
-							.getElementName());
+					txtUMLModel.setText(((IPackageFragment) result[0]).getElementName());
 				}
 			}
-			
+
 			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
 
-	    
-	    /** Future improvement - adding multiple descriptons **
-	     */
-	    Button addDiagrambtn = new Button(container, SWT.NONE);
-	    addDiagrambtn.setText("Add txtUML diagram description");
-	    addDiagrambtn.addSelectionListener(new SelectionListener() {
-	    	
+		/**
+		 * Future improvement - adding multiple descriptions **
+		 */
+		Button addDiagrambtn = new Button(container, SWT.NONE);
+		addDiagrambtn.setText("Add txtUML diagram description");
+		addDiagrambtn.addSelectionListener(new SelectionListener() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				addLayoutField("");
 			}
-			
+
 			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
 
-	    GridData gd = new GridData(SWT.FILL, SWT.FILL, true, false, 2,1);
-	    txtUMLModel.setLayoutData(gd);
-	    txtUMLProject.setLayoutData(gd);
-	    txtUMLLayout.values().forEach( (text) -> text.setLayoutData(GridData.FILL_HORIZONTAL) );
-	    addDiagrambtn.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 4,1)); //adding multiple descriptions
-	    
-	    addInitialLayoutFields();
-	    
-	    sc.setContent(container);
-	    sc.setExpandHorizontal(true);
-	    sc.setExpandVertical(true);
-	    container.setSize(container.computeSize(400, 300, true));
-	    sc.setMinSize(container.getSize());
-	    sc.setSize(container.getSize());
+		GridData gd = new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1);
+		txtUMLModel.setLayoutData(gd);
+		txtUMLProject.setLayoutData(gd);
+		txtUMLLayout.values().forEach((text) -> text.setLayoutData(GridData.FILL_HORIZONTAL));
+		addDiagrambtn.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false, 4, 1));
+		// adding multiple descriptions
 
-	    setControl(parent);
-	    setPageComplete(true);
+		addInitialLayoutFields();
+
+		sc.setContent(container);
+		sc.setExpandHorizontal(true);
+		sc.setExpandVertical(true);
+		container.setSize(container.computeSize(400, 300, true));
+		sc.setMinSize(container.getSize());
+		sc.setSize(container.getSize());
+
+		setControl(parent);
+		setPageComplete(true);
 	}
-	
-	private void addInitialLayoutFields(){
+
+	private void addInitialLayoutFields() {
 		Collection<String> layouts = PreferencesManager.getStrings(PreferencesManager.TXTUML_VISUALIZE_TXTUML_LAYOUT);
-		for(String layout : layouts){
-			if(!layout.isEmpty()) addLayoutField(layout);
+		for (String layout : layouts) {
+			if (!layout.isEmpty())
+				addLayoutField(layout);
 		}
 	}
-	
-	private void addLayoutField(String text){
+
+	private void addLayoutField(String text) {
 		Integer temp_index;
-		try{
-			temp_index = Collections.max(txtUMLLayout.keySet())+1;
-		}catch(NoSuchElementException e){
+		try {
+			temp_index = Collections.max(txtUMLLayout.keySet()) + 1;
+		} catch (NoSuchElementException e) {
 			temp_index = 1;
 		}
-		
+
 		final Integer index = temp_index;
-		
+
 		final Label label = new Label(container, SWT.NONE);
-        label.setText("txtUML Diagram"+index+": ");
-        Control[] children = container.getChildren();
-        label.moveAbove(children[children.length-2]);
-        container.layout(new Control[] {label});
-        
-        final Text textField = new Text(container, SWT.BORDER);
-        textField.setText(text);
-        textField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-        children = container.getChildren();
-        textField.moveAbove(children[children.length-2]);
-        container.layout(new Control[] {textField});
-        
-        final Button browseBtn = new Button(container, SWT.NONE);
-        browseBtn.setText(browseButtonText);
-        browseBtn.addSelectionListener(new SelectionListener() {
-	    	
+		label.setText("txtUML Diagram" + index + ": ");
+		Control[] children = container.getChildren();
+		label.moveAbove(children[children.length - 2]);
+		container.layout(new Control[] { label });
+
+		final Text textField = new Text(container, SWT.BORDER);
+		textField.setText(text);
+		textField.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		children = container.getChildren();
+		textField.moveAbove(children[children.length - 2]);
+		container.layout(new Control[] { textField });
+
+		final Button browseBtn = new Button(container, SWT.NONE);
+		browseBtn.setText(browseButtonText);
+		browseBtn.addSelectionListener(new SelectionListener() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				FilteredTypesSelectionDialog dialog=
-						new FilteredTypesSelectionDialog(container.getShell(),false,
-								PlatformUI.getWorkbench().getProgressService(),
-								SearchEngine.createWorkspaceScope(),
-								IJavaSearchConstants.CLASS_AND_INTERFACE);
+				FilteredTypesSelectionDialog dialog = new FilteredTypesSelectionDialog(container.getShell(), false,
+						PlatformUI.getWorkbench().getProgressService(), SearchEngine.createWorkspaceScope(),
+						IJavaSearchConstants.CLASS_AND_INTERFACE);
 				dialog.open();
 				Object[] result = dialog.getResult();
-				if(result != null && result.length > 0 && result[0] instanceof SourceType){
+				if (result != null && result.length > 0 && result[0] instanceof SourceType) {
 					SourceType item = (SourceType) result[0];
 					textField.setText(item.getFullyQualifiedName());
 				}
 			}
-			
+
 			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
-        children = container.getChildren();
-        browseBtn.moveAbove(children[children.length-2]);
-	    container.layout(new Control[] {browseBtn});
-	    
-        final Button deleteButton = new Button(container, SWT.CENTER);
-        deleteButton.addSelectionListener(new SelectionListener() {
-			
+		children = container.getChildren();
+		browseBtn.moveAbove(children[children.length - 2]);
+		container.layout(new Control[] { browseBtn });
+
+		final Button deleteButton = new Button(container, SWT.CENTER);
+		deleteButton.addSelectionListener(new SelectionListener() {
+
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				label.dispose();
@@ -260,43 +255,47 @@ public class VisualizeTxtUMLPage extends WizardPage {
 				container.layout();
 				txtUMLLayout.remove(index);
 			}
-			
+
 			@Override
-			public void widgetDefaultSelected(SelectionEvent e) {}
+			public void widgetDefaultSelected(SelectionEvent e) {
+			}
 		});
-        deleteButton.setText("X");
-        children = container.getChildren();
-        deleteButton.moveAbove(children[children.length-2]);
-        container.layout(new Control[] {deleteButton});
-        
-        txtUMLLayout.put(index, textField);
-        sc.setMinHeight(container.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
+		deleteButton.setText("X");
+		children = container.getChildren();
+		deleteButton.moveAbove(children[children.length - 2]);
+		container.layout(new Control[] { deleteButton });
+
+		txtUMLLayout.put(index, textField);
+		sc.setMinHeight(container.computeSize(SWT.DEFAULT, SWT.DEFAULT).y);
 	}
 
 	/**
-	   * Returns the txtUML ModelClass
-	   * @return Returns the txtUML ModelClass
-	   */
-	public String getTxtUmlModelClass(){
+	 * Returns the txtUML ModelClass
+	 * 
+	 * @return Returns the txtUML ModelClass
+	 */
+	public String getTxtUmlModelClass() {
 		return txtUMLModel.getText();
 	}
-	
+
 	/**
-	   * Returns the txtUML project's name
-	   * @return Returns the txtUML project's name
-	   */
-	public String getTxtUmlProject(){
+	 * Returns the txtUML project's name
+	 * 
+	 * @return Returns the txtUML project's name
+	 */
+	public String getTxtUmlProject() {
 		return txtUMLProject.getText();
 	}
 
 	/**
 	 * Returns the txtUML model layout
+	 * 
 	 * @return
 	 */
 	public List<String> getTxtUmlLayout() {
 		List<String> result = new LinkedList<String>();
-		for(Text t: txtUMLLayout.values()){
-			if(!"".equals(t.getText())){
+		for (Text t : txtUMLLayout.values()) {
+			if (!"".equals(t.getText())) {
 				result.add(t.getText());
 			}
 		}
