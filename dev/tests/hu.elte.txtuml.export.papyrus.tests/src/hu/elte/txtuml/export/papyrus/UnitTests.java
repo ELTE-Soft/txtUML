414037f4f9b9e535abaf48012ea0f069c2ba3da1
Dobreff András <dobreffandras@gmail.com>
2016-09-27 00:59:46 +0200

    Added UnitTest for StateMachineDiagramElementsProviderImpl

--- dev/tests/hu.elte.txtuml.export.papyrus.tests/src/hu/elte/txtuml/export/papyrus/UnitTests.java
+++ dev/tests/hu.elte.txtuml.export.papyrus.tests/src/hu/elte/txtuml/export/papyrus/UnitTests.java
@@ -5,13 +5,14 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import hu.elte.txtuml.export.papyrus.diagrams.clazz.impl.ClassDiagramElementsManagerTest;
-import hu.elte.txtuml.export.papyrus.diagrams.clazz.impl.ClassDiagramElementsProviderImpl;
 import hu.elte.txtuml.export.papyrus.diagrams.clazz.impl.ClassDiagramElementsProviderImplTest;
 import hu.elte.txtuml.export.papyrus.diagrams.statemachine.impl.StateMachineDiagramElementsManagerTest;
+import hu.elte.txtuml.export.papyrus.diagrams.statemachine.impl.StateMachineDiagramElementsProviderImplTest;
 import hu.elte.txtuml.export.papyrus.elementsarrangers.txtumllayout.LayoutTransformerTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ DiagramManagerUnitTest.class, LayoutTransformerTest.class, ClassDiagramElementsManagerTest.class,
-		StateMachineDiagramElementsManagerTest.class, ClassDiagramElementsProviderImplTest.class })
+		StateMachineDiagramElementsManagerTest.class, ClassDiagramElementsProviderImplTest.class,
+		StateMachineDiagramElementsProviderImplTest.class })
 public class UnitTests {
 }
