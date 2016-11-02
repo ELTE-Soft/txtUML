0cc5a6c73a603b85935dceac8d88fdd8bf25e8cb
Dobreff András <dobreffandras@gmail.com>
2016-11-02 12:01:35 +0100

    Papyrus diagrams open automatically after visualization. (autoformat applied on sourcecode)

--- dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/PapyrusVisualizer.java
+++ dev/plugins/hu.elte.txtuml.export.papyrus/src/hu/elte/txtuml/export/papyrus/PapyrusVisualizer.java
@@ -9,14 +9,16 @@ import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.papyrus.infra.core.resource.ModelMultiException;
 import org.eclipse.papyrus.infra.core.services.ServiceException;
 
+import hu.elte.txtuml.export.papyrus.utils.EditorOpener;
+import hu.elte.txtuml.utils.Logger;
 import hu.elte.txtuml.utils.eclipse.ProjectUtils;
 
 /**
- * Our sample action implements workbench action delegate.
- * The action proxy will be created by the workbench and
- * shown in the UI. When the user tries to use the action,
- * this delegate will be created and execution will be 
- * delegated to it.
+ * Our sample action implements workbench action delegate. The action proxy will
+ * be created by the workbench and shown in the UI. When the user tries to use
+ * the action, this delegate will be created and execution will be delegated to
+ * it.
+ * 
  * @param <ViewPrototype>
  * @see IWorkbenchWindowActionDelegate
  */
@@ -26,78 +28,92 @@ public class PapyrusVisualizer implements IRunnableWithProgress {
 	private String sourceUMLPath;
 	private PapyrusModelManager papyrusModelManager;
 	private Object layoutDescriptor;
-	
+
 	/**
 	 * The constructor.
-	 * @param projectName - The name of the project in which the Papyrus Model will be created.
-	 * @param modelName - The Name of the Papyrus Model
-	 * @param sourceUMLpath - Sourcepath of the Eclipse UML2 model 
-	 * @param layoutDescripton  - A descriptor file which adds constraints to the layout and diagram generation
+	 * 
+	 * @param projectName
+	 *            - The name of the project in which the Papyrus Model will be
+	 *            created.
+	 * @param modelName
+	 *            - The Name of the Papyrus Model
+	 * @param sourceUMLpath
+	 *            - Sourcepath of the Eclipse UML2 model
+	 * @param layoutDescripton
+	 *            - A descriptor file which adds constraints to the layout and
+	 *            diagram generation
 	 */
-	public PapyrusVisualizer(String projectName, String modelName,
-			String sourceUMLpath, Object layoutDescripton) {
+	public PapyrusVisualizer(String projectName, String modelName, String sourceUMLpath, Object layoutDescripton) {
 		this.projectName = projectName;
 		this.modelName = modelName;
 		this.sourceUMLPath = sourceUMLpath;
 		this.layoutDescriptor = layoutDescripton;
 	}
-	
+
 	/**
 	 * @param projectName
 	 * @param modelName
 	 * @param sourceUMLpath
 	 */
-	public PapyrusVisualizer(String projectName, String modelName, String sourceUMLpath){
+	public PapyrusVisualizer(String projectName, String modelName, String sourceUMLpath) {
 		this(projectName, modelName, sourceUMLpath, null);
 	}
-	
+
 	/**
-	 * Executes the visualization process. 
-	 * Creates the project (if not exists) and sets up the Papyrus Model
-	 * @param monitor 
-	 * @return 
-	 * @throws ServiceException 
-	 * @throws ModelMultiException 
+	 * Executes the visualization process. Creates the project (if not exists)
+	 * and sets up the Papyrus Model
+	 * 
+	 * @param monitor
+	 * @return
+	 * @throws ServiceException
+	 * @throws ModelMultiException
 	 */
 	@Override
-	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException{
-		monitor.beginTask("Visualization", 100);			
+	public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+		monitor.beginTask("Visualization", 100);
 		monitor.subTask("Creating new Papyrus project...");
 
 		IProject project = ProjectUtils.createProject(projectName);
 		ProjectUtils.openProject(project);
-		
-		
+
 		monitor.worked(20);
-		try{	
+		try {
 			createAndOpenPapyrusModel(new SubProgressMonitor(monitor, 80));
-		}catch(Exception e){
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
-	
-	
+
 	/**
-	 * Creates the Papyrus Model and fills the diagrams.
-	 * If the Model already exists, then loads it.
-	 * @throws ModelMultiException - If the loading of existing model fails
-	 * @throws ServiceException 
+	 * Creates the Papyrus Model and fills the diagrams. If the Model already
+	 * exists, then loads it.
+	 * 
+	 * @throws ModelMultiException
+	 *             - If the loading of existing model fails
+	 * @throws ServiceException
 	 */
-	private void createAndOpenPapyrusModel(IProgressMonitor monitor) throws ModelMultiException, ServiceException, InvocationTargetException, InterruptedException {
+	private void createAndOpenPapyrusModel(IProgressMonitor monitor)
+			throws ModelMultiException, ServiceException, InvocationTargetException, InterruptedException {
 		monitor.beginTask("Generating Papyrus Model", 100);
 		PapyrusModelCreator papyrusModelCreator = new PapyrusModelCreator(projectName + "/" + modelName);
 		papyrusModelCreator.setUpUML(sourceUMLPath);
-		if(!papyrusModelCreator.diExists()){
-			
+		if (!papyrusModelCreator.diExists()) {
+
 			monitor.subTask("Generating Papyrus model...");
 			papyrusModelCreator.createPapyrusModel();
-		
-			
-					papyrusModelManager = new PapyrusModelManager(papyrusModelCreator.getServiceRegistry());
-					papyrusModelManager.setLayoutController(layoutDescriptor);
-					monitor.worked(10);
-					
-					papyrusModelManager.createAndFillDiagrams(new SubProgressMonitor(monitor, 90));
-		} 
+
+			papyrusModelManager = new PapyrusModelManager(papyrusModelCreator.getServiceRegistry());
+			papyrusModelManager.setLayoutController(layoutDescriptor);
+			monitor.worked(10);
+
+			papyrusModelManager.createAndFillDiagrams(new SubProgressMonitor(monitor, 90));
+		}
+
+		if (papyrusModelCreator.diExists()) {
+			EditorOpener.openPapyrusEditor(papyrusModelCreator.getDi());
+		} else {
+			Logger.user.error(".di File does not exist. Papyrus Model can't be opened.");
+		}
+
 	}
 }
