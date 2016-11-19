ec38cc37d10dd5049452ba4bb3db391aaedda77c
Dávid J. Németh <djnemeth@outlook.com>
2016-11-19 22:29:38 +0100

    Add unit tests for scoping

--- dev/tests/hu.elte.txtuml.xtxtuml.tests/src/hu/elte/txtuml/xtxtuml/tests/UnitTests.java
+++ dev/tests/hu.elte.txtuml.xtxtuml.tests/src/hu/elte/txtuml/xtxtuml/tests/UnitTests.java
@@ -6,11 +6,12 @@ import org.junit.runners.Suite.SuiteClasses;
 
 import hu.elte.txtuml.xtxtuml.tests.compiler.XtxtUMLCompilerTests;
 import hu.elte.txtuml.xtxtuml.tests.parser.XtxtUMLParserTests;
+import hu.elte.txtuml.xtxtuml.tests.scoping.XtxtUMLScopingTests;
 import hu.elte.txtuml.xtxtuml.tests.typesystem.XtxtUMLTypeComputerTests;
 import hu.elte.txtuml.xtxtuml.tests.validation.XtxtUMLValidationTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({ XtxtUMLValidationTests.class, XtxtUMLCompilerTests.class, XtxtUMLParserTests.class,
-		XtxtUMLTypeComputerTests.class })
+		XtxtUMLTypeComputerTests.class, XtxtUMLScopingTests.class })
 public class UnitTests {
 }
