2e211a5d12dfefb7107a57a23068df6cb117a9ac
Dobreff András <dobreffandras@gmail.com>
2017-01-14 17:45:54 +0100

    Deprecated integration tests eliminated

--- dev/tests/hu.elte.txtuml.export.papyrus.tests/src/hu/elte/txtuml/export/papyrus/IntegrationTests.java
+++ dev/tests/hu.elte.txtuml.export.papyrus.tests/src/hu/elte/txtuml/export/papyrus/IntegrationTests.java
@@ -5,8 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({PapyrusModelCreatorTest.class, ProjectUtilsTest.class,
-	DiagramManagerIntegrationTest.class})
+@SuiteClasses({PapyrusModelCreatorTest.class, ProjectUtilsTest.class})
 
 public class IntegrationTests {
 }
