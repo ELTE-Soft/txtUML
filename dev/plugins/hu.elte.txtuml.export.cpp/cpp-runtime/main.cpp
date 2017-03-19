3312c914cc84db8d4bf2d17dec1b8616a586c7a3
Nagy András <nagyandras95@inf.elte.hu>
2017-03-19 19:16:16 +0100

    Add missing deployment iclude from main.cpp

--- dev/plugins/hu.elte.txtuml.export.cpp/cpp-runtime/main.cpp
+++ dev/plugins/hu.elte.txtuml.export.cpp/cpp-runtime/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 #include "Env.hpp"
+#include "deployment.hpp"
 
 int main()
 {
   Env::initEnvironment();
   UsedRuntimePtr rt = UsedRuntimeType::getRuntimeInstance();
-  //rt->startRT(); // the runtime could be started later
+  //rt->startRT(); // the runtime can be started later
 
   std::cout << "The files are successfully compiled!\nYou can now modify the main.cpp" << std::endl;
   std::cin.get();
