5635d55240fb57ea37e090689c02298b492a61e4
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2017-04-10 09:03:59 +0200

    Fixed minor documentation problem.

--- dev/plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/DataType.java
+++ dev/plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/DataType.java
@@ -47,7 +47,7 @@ import java.util.Objects;
  * to set the default values of its fields</li>
  * <li><i>Fields:</i> allowed, only with parameters of types which are data
  * types, model enums, external classes or primitives (including {@code String}
- * ); they represent attributes of the model class</li>
+ * ); they represent attributes of the data type</li>
  * <li><i>Methods:</i> allowed, only with parameters and return values of types
  * which are subclasses of <code>ModelClass</code>, signals, data types, model
  * enums, external classes or primitives (including {@code String}); they
