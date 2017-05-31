9bc032bc5574ad51dd606662e4c4c1d85eb65a27
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2017-05-31 23:46:26 +0200

    Updated the layout export tests to be compatible with the new collection framework.

--- dev/tests/hu.elte.txtuml.layout.export.tests/src/hu/elte/txtuml/layout/export/model2/associations/DiamondLeft_DiamondRight.java
+++ dev/tests/hu.elte.txtuml.layout.export.tests/src/hu/elte/txtuml/layout/export/model2/associations/DiamondLeft_DiamondRight.java
@@ -1,13 +1,14 @@
 package hu.elte.txtuml.layout.export.model2.associations;
 
 import hu.elte.txtuml.api.model.Association;
+import hu.elte.txtuml.api.model.One;
 import hu.elte.txtuml.layout.export.model2.DiamondLeft;
 import hu.elte.txtuml.layout.export.model2.DiamondRight;
 
 public class DiamondLeft_DiamondRight extends Association {
-	public class end1 extends One<DiamondLeft> {
+	public class end1 extends End<One<DiamondLeft>> {
 	}
 
-	public class end2 extends One<DiamondRight> {
+	public class end2 extends End<One<DiamondRight>> {
 	}
 }
--- dev/tests/hu.elte.txtuml.layout.export.tests/src/hu/elte/txtuml/layout/export/model2/associations/DiamondTop_DiamondRight.java
+++ dev/tests/hu.elte.txtuml.layout.export.tests/src/hu/elte/txtuml/layout/export/model2/associations/DiamondTop_DiamondRight.java
@@ -1,13 +1,14 @@
 package hu.elte.txtuml.layout.export.model2.associations;
 
 import hu.elte.txtuml.api.model.Association;
+import hu.elte.txtuml.api.model.One;
 import hu.elte.txtuml.layout.export.model2.DiamondRight;
 import hu.elte.txtuml.layout.export.model2.DiamondTop;
 
 public class DiamondTop_DiamondRight extends Association {
-	public class end1 extends One<DiamondTop> {
+	public class end1 extends End<One<DiamondTop>> {
 	}
 
-	public class end2 extends One<DiamondRight> {
+	public class end2 extends End<One<DiamondRight>> {
 	}
 }
