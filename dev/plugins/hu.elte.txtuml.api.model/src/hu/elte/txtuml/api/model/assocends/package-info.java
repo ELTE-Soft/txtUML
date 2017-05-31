60cc897be855fc448801167bcb504ec026175945
Gábor Ferenc Kovács <kovacsgabor@users.noreply.github.com>
2017-05-31 23:34:08 +0200

    Unnecessary classes of the old collection framework removed.

--- dev/plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/assocends/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Contains interfaces which provide an abstraction to association end
- * properties, like navigability and multiplicity.
- * <p>
- * See the documentation of {@link hu.elte.txtuml.api.model.Model} for an
- * overview on modeling in JtxtUML.
- */
-package hu.elte.txtuml.api.model.assocends;
