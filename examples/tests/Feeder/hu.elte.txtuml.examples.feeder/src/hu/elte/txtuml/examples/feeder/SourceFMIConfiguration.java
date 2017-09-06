5597eeca591c7e913fe624bcd8ca8155ceb0e151
nboldi <nboldi@elte.hu>
2017-09-06 12:59:59 +0200

    Changed InitialReal to InitialInteger

--- examples/tests/Feeder/hu.elte.txtuml.examples.feeder/src/hu/elte/txtuml/examples/feeder/SourceFMIConfiguration.java
+++ examples/tests/Feeder/hu.elte.txtuml.examples.feeder/src/hu/elte/txtuml/examples/feeder/SourceFMIConfiguration.java
@@ -4,7 +4,7 @@ import hu.elte.txtuml.api.deployment.fmi.FMIConfiguration;
 import hu.elte.txtuml.api.deployment.fmi.FMU;
 import hu.elte.txtuml.api.deployment.fmi.FMUInput;
 import hu.elte.txtuml.api.deployment.fmi.FMUOutput;
-import hu.elte.txtuml.api.deployment.fmi.InitialRealValue;
+import hu.elte.txtuml.api.deployment.fmi.InitialIntegerValue;
 import hu.elte.txtuml.examples.feeder.model.RequestSignal;
 import hu.elte.txtuml.examples.feeder.model.ResponseSignal;
 import hu.elte.txtuml.examples.feeder.model.Source;
@@ -12,7 +12,7 @@ import hu.elte.txtuml.examples.feeder.model.Source;
 @FMU(fmuClass = Source.class)
 @FMUInput(inputSignal = RequestSignal.class)
 @FMUOutput(outputSignal = ResponseSignal.class)
-@InitialRealValue(variableName = "amount", value = 0)
+@InitialIntegerValue(variableName = "amount", value = 0)
 public class SourceFMIConfiguration extends FMIConfiguration {
 
 }
