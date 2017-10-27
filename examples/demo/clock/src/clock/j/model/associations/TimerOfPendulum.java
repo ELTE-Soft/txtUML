bf11c4c3886ef22e5253ab5d2e6d7eb7a2052aee
kovacsgabor <kovacsgabor@users.noreply.github.com>
2017-10-27 15:45:55 +0200

    Fixed errors after the merge with the new externals in clock demo.

--- examples/demo/clock/src/clock/j/model/associations/TimerOfPendulum.java
+++ examples/demo/clock/src/clock/j/model/associations/TimerOfPendulum.java
@@ -2,9 +2,11 @@ package clock.j.model.associations;
 
 import clock.j.model.classes.Pendulum;
 import hu.elte.txtuml.api.model.Association;
+import hu.elte.txtuml.api.model.One;
+import hu.elte.txtuml.api.model.ZeroToOne;
 import hu.elte.txtuml.api.stdlib.timers.Timer;
 
 public class TimerOfPendulum extends Association {
-	public class timer extends One<Timer> {}
-	public class pendulum extends HiddenMaybeOne<Pendulum> {}
+	public class timer extends End<One<Timer>> {}
+	public class pendulum extends HiddenEnd<ZeroToOne<Pendulum>> {}
 }
