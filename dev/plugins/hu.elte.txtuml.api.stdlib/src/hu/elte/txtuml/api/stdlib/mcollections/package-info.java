754d7966d86a8765613c4f5c9b5523826120bd8c
kovacsgabor <kovacsgabor@users.noreply.github.com>
2017-10-27 15:40:59 +0200

    MCollections stdlib model removed. This feature cannot be applied to the new externals as templates are not yet supported in txtUML.

--- dev/plugins/hu.elte.txtuml.api.stdlib/src/hu/elte/txtuml/api/stdlib/mcollections/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Provides access to mutable collections in txtUML models.
- */
-@Model("txtUML MCollections")
-package hu.elte.txtuml.api.stdlib.mcollections;
-
-import hu.elte.txtuml.api.model.Model;
