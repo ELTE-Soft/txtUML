874a1c48dce19b7840877eab39a1103fd48da62b
Nagy Andras <nagyandras95@inf.elte.hu>
2018-04-21 11:24:25 +0200

    Prepare x pingpong demo to export with ports

---
+++ examples/demo/pingpong/src/pingpong/x/cpp/PingPongConfiguration.java
@@ -0,0 +1,15 @@
+package pingpong.x.cpp;
+
+import hu.elte.txtuml.api.deployment.Configuration;
+import hu.elte.txtuml.api.deployment.Group;
+import hu.elte.txtuml.api.deployment.Runtime;
+import hu.elte.txtuml.api.deployment.RuntimeType;
+import pingpong.j.model.Game;
+import pingpong.j.model.Player;
+import pingpong.j.model.Racket;
+
+@Group(contains = { Player.class, Game.class, Racket.class })
+@Runtime(RuntimeType.SINGLE)
+public class PingPongConfiguration extends Configuration {
+
+}
