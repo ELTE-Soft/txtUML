2641e31baef1949ae35731f3ab182962efcd16f8
Krisztián Mózsi <mozsik0@gmail.com>
2018-04-22 13:35:25 +0200

    Fix imports in pingpong demo

--- examples/demo/pingpong/src/pingpong/x/cpp/PingPongConfiguration.java
+++ examples/demo/pingpong/src/pingpong/x/cpp/PingPongConfiguration.java
@@ -4,9 +4,9 @@ import hu.elte.txtuml.api.deployment.Configuration;
 import hu.elte.txtuml.api.deployment.Group;
 import hu.elte.txtuml.api.deployment.Runtime;
 import hu.elte.txtuml.api.deployment.RuntimeType;
-import pingpong.j.model.Game;
-import pingpong.j.model.Player;
-import pingpong.j.model.Racket;
+import pingpong.x.model.Game;
+import pingpong.x.model.Player;
+import pingpong.x.model.Racket;
 
 @Group(contains = { Player.class, Game.class, Racket.class })
 @Runtime(RuntimeType.SINGLE)
