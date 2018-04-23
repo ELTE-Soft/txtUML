0d849136a18dd17eb8f14a650317739ad27b87b4
Tibor Bodorkós <bodorkostibi@caesar.elte.hu>
2018-04-23 22:20:50 +0200

    Adds environment not found exception

---
+++ dev/plugins/hu.elte.txtuml.export.cpp/src/hu/elte/txtuml/export/cpp/EnvironmentNotFoundException.java
@@ -0,0 +1,8 @@
+package hu.elte.txtuml.export.cpp;
+
+@SuppressWarnings("serial")
+public class EnvironmentNotFoundException extends Exception {
+	  public EnvironmentNotFoundException(String message) {
+		  super(message);
+	  }
+}
