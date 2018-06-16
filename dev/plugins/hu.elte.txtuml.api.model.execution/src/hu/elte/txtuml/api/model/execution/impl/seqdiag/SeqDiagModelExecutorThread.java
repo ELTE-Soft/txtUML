ELTE-Soft/txtUML

commit 573affad6c8bd1104a6283e7ce51412c1ef35642

Sequence diagram runtime refacts

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/SeqDiagModelExecutorThread.java
@@ -33,6 +33,11 @@ class SeqDiagModelExecutorThread extends FIFOExecutorThread implements ExecutorT
 	}
 
 	@Override
+	public DefaultSeqDiagRuntime getModelRuntime() {
+		return (DefaultSeqDiagRuntime) super.getModelRuntime();
+	}
+
+	@Override
 	public void doRun() {
 		super.doRun();
 		root.kill();
@@ -92,8 +97,8 @@ class SeqDiagModelExecutorThread extends FIFOExecutorThread implements ExecutorT
 	 * added.
 	 */
 	public void assertState(ModelClass instance, Class<?> state) {
-		Class<?> currentState = ((SeqDiagModelClassRuntime) ((DefaultSeqDiagRuntime) getModelRuntime())
-				.getRuntimeOf(instance)).getCurrentState();
+		DefaultSeqDiagModelClassRuntime modelClassRuntime = getModelRuntime().getRuntimeOf(instance);
+		Class<?> currentState = modelClassRuntime.getCurrentState();
 		if (!currentState.equals(state)) {
 			root.getExecutor()
 					.addError(new ModelStateAssertError(state.getCanonicalName(), currentState.getCanonicalName()));

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/DefaultSeqDiagModelClassRuntime.java
@@ -11,9 +11,9 @@ import hu.elte.txtuml.api.model.execution.impl.singlethread.SingleThreadModelCla
  * so it may not be used in a multi-threaded executor as it lacks the necessary
  * synchronizations.
  */
-class SeqDiagModelClassRuntime extends SingleThreadModelClassRuntime {
+class DefaultSeqDiagModelClassRuntime extends SingleThreadModelClassRuntime {
 
-	public SeqDiagModelClassRuntime(ModelClass wrapped, AbstractExecutorThread thread) {
+	public DefaultSeqDiagModelClassRuntime(ModelClass wrapped, AbstractExecutorThread thread) {
 		super(wrapped, thread);
 	}
 

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/DefaultSeqDiagRuntime.java
@@ -17,7 +17,7 @@ import hu.elte.txtuml.api.model.seqdiag.SequenceDiagram;
  * thread.
  */
 @SequenceDiagramRelated
-public class DefaultSeqDiagRuntime extends SingleThreadModelRuntime<SeqDiagModelClassRuntime, SingleThreadPortRuntime> {
+class DefaultSeqDiagRuntime extends SingleThreadModelRuntime<DefaultSeqDiagModelClassRuntime, SingleThreadPortRuntime> {
 
 	private final SeqDiagModelExecutorThread modelThread;
 
@@ -60,13 +60,18 @@ public class DefaultSeqDiagRuntime extends SingleThreadModelRuntime<SeqDiagModel
 	// The required methods of an implementer of the SinglethreadModelRuntime
 
 	@Override
+	protected DefaultSeqDiagModelClassRuntime getRuntimeOf(ModelClass cls) {
+		return (DefaultSeqDiagModelClassRuntime) super.getRuntimeOf(cls);
+	}
+
+	@Override
 	public DefaultSeqDiagExecutor getExecutor() {
 		return (DefaultSeqDiagExecutor) super.getExecutor();
 	}
 
 	@Override
-	public SeqDiagModelClassRuntime createModelClassRuntime(ModelClass object) {
-		return new SeqDiagModelClassRuntime(object, modelThread);
+	public DefaultSeqDiagModelClassRuntime createModelClassRuntime(ModelClass object) {
+		return new DefaultSeqDiagModelClassRuntime(object, modelThread);
 	}
 
 	@Override
@@ -82,4 +87,5 @@ public class DefaultSeqDiagRuntime extends SingleThreadModelRuntime<SeqDiagModel
 	SeqDiagModelExecutorThread getModelThread() {
 		return modelThread;
 	}
+
 }

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/InteractionThread.java
@@ -32,7 +32,7 @@ import hu.elte.txtuml.utils.Logger;
  * inner invariants described at {@link #currentMessage}.
  */
 @SequenceDiagramRelated
-public class InteractionThread extends AbstractModelExecutor.OwnedThread<DefaultSeqDiagExecutor>
+class InteractionThread extends AbstractModelExecutor.OwnedThread<DefaultSeqDiagExecutor>
 		implements InteractionRuntime, SeqDiagThread {
 
 	/*

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/InvalidMessageSentListener.java
@@ -12,7 +12,7 @@ import hu.elte.txtuml.api.model.impl.SequenceDiagramRelated;
  * Reports errors when messages are lost or arrive to deleted model objects.
  */
 @SequenceDiagramRelated
-public class InvalidMessageSentListener implements WarningListener {
+class InvalidMessageSentListener implements WarningListener {
 	private final DefaultSeqDiagExecutor executor;
 
 	public InvalidMessageSentListener(DefaultSeqDiagExecutor executor) {

commit c43431c6c0e027284becd1d66bc82c54bd3aae31

Simplify the implementation of state asserts
Due to the new abstractions introduced in 573affa, the implementation is
actually cleaner without named subexpressions.

dev/plugins/hu.elte.txtuml.api.model.execution/src/hu/elte/txtuml/api/model/execution/impl/seqdiag/SeqDiagModelExecutorThread.java
@@ -97,8 +97,7 @@ class SeqDiagModelExecutorThread extends FIFOExecutorThread implements ExecutorT
 	 * added.
 	 */
 	public void assertState(ModelClass instance, Class<?> state) {
-		DefaultSeqDiagModelClassRuntime modelClassRuntime = getModelRuntime().getRuntimeOf(instance);
-		Class<?> currentState = modelClassRuntime.getCurrentState();
+		Class<?> currentState = getModelRuntime().getRuntimeOf(instance).getCurrentState();
 		if (!currentState.equals(state)) {
 			root.getExecutor()
 					.addError(new ModelStateAssertError(state.getCanonicalName(), currentState.getCanonicalName()));