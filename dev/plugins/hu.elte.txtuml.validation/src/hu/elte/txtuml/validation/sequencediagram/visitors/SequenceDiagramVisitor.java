684bd599b9cb42af1ede745d148cc6b88ed2ab6d
Jánosi Tamás <janosi96@gmail.com>
2018-11-27 17:58:11 +0100

    Start to reimplement validation
    Old methods are removed, loops and opt/alt fragments are beeing checked.
    Checking par fragments, method invocations and Interaction declarations
    as variables are still missing.

--- dev/plugins/hu.elte.txtuml.validation/src/hu/elte/txtuml/validation/sequencediagram/visitors/SequenceDiagramVisitor.java
+++ dev/plugins/hu.elte.txtuml.validation/src/hu/elte/txtuml/validation/sequencediagram/visitors/SequenceDiagramVisitor.java
@@ -1,23 +1,36 @@
 package hu.elte.txtuml.validation.sequencediagram.visitors;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
+import org.eclipse.jdt.core.dom.Annotation;
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.Expression;
+import org.eclipse.jdt.core.dom.DoStatement;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
-import org.eclipse.jdt.core.dom.LambdaExpression;
+import org.eclipse.jdt.core.dom.ForStatement;
+import org.eclipse.jdt.core.dom.IfStatement;
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-import org.eclipse.jdt.core.dom.MethodInvocation;
+import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
+import org.eclipse.jdt.core.dom.Statement;
+import org.eclipse.jdt.core.dom.WhileStatement;
 
+import hu.elte.txtuml.api.model.ModelClass;
+import hu.elte.txtuml.utils.jdt.ElementTypeTeller;
 import hu.elte.txtuml.validation.common.ProblemCollector;
+import hu.elte.txtuml.validation.sequencediagram.SequenceErrors;
 
 /**
  * Visitor for sequence diagram description validation.
  */
+@SuppressWarnings("unchecked")
 public class SequenceDiagramVisitor extends ASTVisitor {
 
-	protected ProblemCollector collector;
+	private ProblemCollector collector;
+	private ASTNode placeOfError;
 
 	public SequenceDiagramVisitor(ProblemCollector collector) {
 		this.collector = collector;
@@ -25,40 +38,165 @@ public class SequenceDiagramVisitor extends ASTVisitor {
 
 	@Override
 	public boolean visit(CompilationUnit node) {
-		Utils.acceptSequenceDiagrams(node, this);
+		List<AbstractTypeDeclaration> sequenceDiagrams = Utils.getSequenceDiagrams(node);
+		sequenceDiagrams.forEach(diag -> diag.accept(this));
 		return false;
 	}
 
 	@Override
 	public boolean visit(FieldDeclaration elem) {
-		Utils.checkFieldDeclaration(collector, elem);
+		checkFieldDeclaration(elem);
 		return false;
 	}
 
 	@Override
 	public boolean visit(MethodDeclaration elem) {
-		return elem.getName().getFullyQualifiedName().equals("run");
-	}
-
-	@Override
-	public boolean visit(MethodInvocation elem) {
-		if (elem.resolveMethodBinding().getName().equals("par")) {
-			Utils.checkEmptyPar(collector, elem);
+		if (elem.getName().getFullyQualifiedName().equals("run")) {
+			checkSendInRun(elem);
 			return true;
 		}
-		ASTNode grandParent = elem.getParent().getParent();
-		if (grandParent instanceof MethodInvocation
-				&& ((MethodInvocation) grandParent).resolveMethodBinding().getName().equals("par")) {
-			Utils.checkSendInLambda(collector, elem, this);
-		}
-		Utils.checkInvalidActionCall(elem, collector);
 		return false;
 	}
 
-	@Override
-	public boolean visit(Block node) {
-		Utils.checkSendExists(collector, node, this);
-		return true;
+	private void checkSendInRun(MethodDeclaration node) {
+		final boolean showErrorHere = placeOfError == null;
+		if (showErrorHere) {
+			placeOfError = node.getBody();
+		}
+		List<Boolean> childChecks = new ArrayList<>();
+		List<Statement> statements = (List<Statement>) node.getBody().statements();
+		List<Statement> loops = Utils.getLoopNodes(statements);
+		loops.forEach(loop -> {
+			if (showErrorHere) {
+				placeOfError = node.getBody();
+			}
+			childChecks.add(checkSendInLoopNode(loop));
+		});
+		List<IfStatement> ifNodes = Utils.getIfNodes(statements);
+		ifNodes.forEach(ifNode -> {
+			if (showErrorHere) {
+				placeOfError = node.getBody();
+			}
+			childChecks.add(checkSendInIfNode(ifNode));
+		});
+		if (childChecks.isEmpty()) {
+			SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), placeOfError);
+		}
+	}
+
+	private boolean checkSendInLoopNode(Statement loopNode) {
+		Statement body;
+		if (loopNode instanceof WhileStatement) {
+			WhileStatement whileLoop = (WhileStatement) loopNode;
+			body = whileLoop.getBody();
+		} else if (loopNode instanceof ForStatement) {
+			ForStatement forLoop = (ForStatement) loopNode;
+			body = forLoop.getBody();
+		} else {
+			DoStatement doWhileLoop = (DoStatement) loopNode;
+			body = doWhileLoop.getBody();
+		}
+		if (placeOfError == loopNode.getParent() || placeOfError == loopNode) {
+			placeOfError = body;
+		}
+		if (body instanceof Block) {
+			return checkSendInBlock((Block) body);
+		} else {
+			return checkSendInStatement(body);
+		}
+
+	}
+
+	private boolean checkSendInIfNode(IfStatement node) {
+		boolean isValid = true;
+		boolean showErrorHere = placeOfError == node.getParent() || placeOfError == node;
+		Statement thenStatement = node.getThenStatement();
+		if (showErrorHere) {
+			placeOfError = thenStatement;
+		}
+		if (thenStatement instanceof Block) {
+			isValid = isValid && checkSendInBlock((Block) thenStatement);
+		} else {
+			isValid = isValid && checkSendInStatement(thenStatement);
+		}
+		Statement elseStatement = node.getElseStatement();
+		if (showErrorHere) {
+			placeOfError = elseStatement;
+		}
+		if (elseStatement instanceof IfStatement) {
+			isValid = isValid && checkSendInIfNode((IfStatement) elseStatement);
+		} else if (elseStatement instanceof Block) {
+			isValid = isValid && checkSendInBlock((Block) elseStatement);
+		} else {
+			isValid = isValid && checkSendInStatement(elseStatement);
+		}
+		return isValid;
+	}
+
+	private boolean checkSendInBlock(Block node) {
+		final boolean showErrorHere = placeOfError == node;
+		List<Statement> statements = (List<Statement>) node.statements();
+		List<Statement> loops = Utils.getLoopNodes(statements);
+		List<Boolean> childChecks = new ArrayList<>();
+		loops.forEach(loop -> {
+			if (showErrorHere) {
+				placeOfError = node;
+			}
+			childChecks.add(checkSendInLoopNode(loop));
+		});
+		List<IfStatement> ifNodes = Utils.getIfNodes(statements);
+		ifNodes.forEach(ifNode -> {
+			if (showErrorHere) {
+				placeOfError = node;
+			}
+			childChecks.add(checkSendInIfNode(ifNode));
+		});
+		if (childChecks.isEmpty()) {
+			SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), placeOfError);
+			return false;
+		}
+		return !childChecks.contains(false);
+	}
+
+	private boolean checkSendInStatement(Statement node) {
+		if (Utils.isLoopNode(node)) {
+			return checkSendInLoopNode(node);
+		} else if (node instanceof IfStatement) {
+			return checkSendInIfNode((IfStatement) node);
+		} else {
+			SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), placeOfError);
+			return false;
+		}
+	}
+
+	private void checkFieldDeclaration(FieldDeclaration elem) {
+		List<?> modifiers = elem.modifiers();
+		for (Object modifier : modifiers) {
+			if (modifier instanceof Annotation) {
+				Annotation ca = (Annotation) modifier;
+				if (ca.getTypeName().getFullyQualifiedName().equals("Position")) {
+					// position value
+					SingleMemberAnnotation position;
+					try {
+						position = (SingleMemberAnnotation) ca;
+					} catch (ClassCastException ex) {
+						return;
+					}
+					int annotationVal = (int) position.getValue().resolveConstantExpressionValue();
+					if (annotationVal < 0) {
+						collector.report(SequenceErrors.INVALID_POSITION.create(collector.getSourceInfo(), position));
+					}
+
+					// type of annotated field
+					boolean isModelClass = ElementTypeTeller.hasSuperClass(elem.getType().resolveBinding(),
+							ModelClass.class.getCanonicalName());
+					if (!isModelClass) {
+						collector.report(SequenceErrors.INVALID_LIFELINE_DECLARATION.create(collector.getSourceInfo(),
+								elem.getType()));
+					}
+				}
+			}
+		}
 	}
 
 }
--- dev/plugins/hu.elte.txtuml.validation/src/hu/elte/txtuml/validation/sequencediagram/visitors/Utils.java
+++ dev/plugins/hu.elte.txtuml.validation/src/hu/elte/txtuml/validation/sequencediagram/visitors/Utils.java
@@ -4,175 +4,53 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.List;
 
-import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.IJavaElement;
-import org.eclipse.jdt.core.dom.AST;
-import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.AbstractTypeDeclaration;
-import org.eclipse.jdt.core.dom.Annotation;
-import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.DoStatement;
-import org.eclipse.jdt.core.dom.Expression;
-import org.eclipse.jdt.core.dom.ExpressionStatement;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.ForStatement;
 import org.eclipse.jdt.core.dom.IMethodBinding;
 import org.eclipse.jdt.core.dom.IfStatement;
-import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.MethodInvocation;
-import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.WhileStatement;
 
-import hu.elte.txtuml.api.model.Action;
-import hu.elte.txtuml.api.model.ModelClass;
 import hu.elte.txtuml.api.model.seqdiag.Sequence;
 import hu.elte.txtuml.api.model.seqdiag.SequenceDiagram;
-import hu.elte.txtuml.utils.Logger;
 import hu.elte.txtuml.utils.jdt.ElementTypeTeller;
-import hu.elte.txtuml.validation.common.ProblemCollector;
-import hu.elte.txtuml.validation.sequencediagram.SequenceErrors;
 
 /**
  * Utils for sequence diagram validation.
  */
-@SuppressWarnings("unchecked")
 public class Utils {
 
-	public static void acceptSequenceDiagrams(CompilationUnit node, ASTVisitor visitor) {
+	@SuppressWarnings("unchecked")
+	public static List<AbstractTypeDeclaration> getSequenceDiagrams(CompilationUnit node) {
 		List<AbstractTypeDeclaration> types = node.types();
-		types.stream().map(td -> ((AbstractTypeDeclaration) td)).filter(
-				td -> ElementTypeTeller.hasSuperClass(td.resolveBinding(), SequenceDiagram.class.getCanonicalName()))
-				.forEach(td -> td.accept(visitor));
+		List<AbstractTypeDeclaration> sequenceDiagrams = types.stream()
+				.map(td -> ((AbstractTypeDeclaration) td)).filter(td -> ElementTypeTeller
+						.hasSuperClass(td.resolveBinding(), SequenceDiagram.class.getCanonicalName()))
+				.collect(toList());
+		return sequenceDiagrams;
 	}
 
-	public static void checkFieldDeclaration(ProblemCollector collector, FieldDeclaration elem) {
-		List<?> modifiers = elem.modifiers();
-		for (Object modifier : modifiers) {
-			if (modifier instanceof Annotation) {
-				Annotation ca = (Annotation) modifier;
-				if (ca.getTypeName().getFullyQualifiedName().equals("Position")) {
-					// position value
-					SingleMemberAnnotation position;
-					try {
-						position = (SingleMemberAnnotation) ca;
-					} catch (ClassCastException ex) {
-						return;
-					}
-					int annotationVal = (int) position.getValue().resolveConstantExpressionValue();
-					if (annotationVal < 0) {
-						collector.report(SequenceErrors.INVALID_POSITION.create(collector.getSourceInfo(), position));
-					}
-
-					// type of annotated field
-					boolean isModelClass = ElementTypeTeller.hasSuperClass(elem.getType().resolveBinding(),
-							ModelClass.class.getCanonicalName());
-					if (!isModelClass) {
-						collector.report(SequenceErrors.INVALID_LIFELINE_DECLARATION.create(collector.getSourceInfo(),
-								elem.getType()));
-					}
-				}
-			}
-		}
+	public static List<Statement> getLoopNodes(List<Statement> statements) {
+		List<Statement> loops = statements.stream().filter(Utils::isLoopNode).collect(toList());
+		return loops;
 	}
 
-	public static void checkInvalidActionCall(MethodInvocation elem, ProblemCollector collector) {
-		boolean isActionCall;
-		try {
-			isActionCall = elem.resolveMethodBinding().getDeclaringClass().getQualifiedName()
-					.equals(Action.class.getCanonicalName());
-		} catch (NullPointerException ex) {
-			isActionCall = false;
-		}
-		if (isActionCall) {
-			collector.report(SequenceErrors.INVALID_ACTION_CALL.create(collector.getSourceInfo(), elem));
-		}
+	public static List<IfStatement> getIfNodes(List<Statement> statements) {
+		List<IfStatement> ifNodes = statements.stream().filter(stm -> stm instanceof IfStatement)
+				.map(stm -> (IfStatement) stm).collect(toList());
+		return ifNodes;
 	}
 
-	public static void checkSendExists(ProblemCollector collector, Block node, ASTVisitor visitor) {
-		List<Statement> statements = (List<Statement>) node.statements();
-		boolean isLeaf = !statements.stream().anyMatch(stm -> stm instanceof WhileStatement
-				|| stm instanceof IfStatement || stm instanceof ForStatement || stm instanceof DoStatement);
-		if (!isLeaf) {
-			return;
-		}
-		List<MethodInvocation> methodInvocations = statements.stream().map(stm -> ((Statement) stm))
-				.filter(stm -> stm instanceof ExpressionStatement).map(stm -> (ExpressionStatement) stm)
-				.map(ExpressionStatement::getExpression).filter(expr -> expr instanceof MethodInvocation)
-				.map(expr -> (MethodInvocation) expr).collect(toList());
-		isLeaf = !methodInvocations.stream().map(inv -> inv.resolveMethodBinding().getName())
-				.anyMatch(name -> name.equals("par"));
-		if (!isLeaf) {
-			return;
-		}
-		boolean containsSend = methodInvocations.stream().anyMatch(Utils::isSendInvocation);
-		if (!containsSend) {
-			boolean isAllSequenceMethod = methodInvocations.stream().allMatch(inv -> {
-				try {
-					return inv.resolveMethodBinding().getDeclaringClass().getQualifiedName()
-							.equals(Sequence.class.getCanonicalName());
-				} catch (NullPointerException ex) {
-					return false;
-				}
-			});
-			if (methodInvocations.isEmpty() || isAllSequenceMethod) {
-				collector.report(SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), node));
-			}
-			List<MethodInvocation> notSequenceInvocations = methodInvocations.stream().filter(inv -> {
-				try {
-					String declaringClass = inv.resolveMethodBinding().getDeclaringClass().getQualifiedName();
-					return !declaringClass.equals(Sequence.class.getCanonicalName())
-							&& !declaringClass.equals(Action.class.getCanonicalName());
-				} catch (NullPointerException ex) {
-					return false;
-				}
-			}).collect(toList());
-			notSequenceInvocations.forEach(inv -> validateInvokedMethod(inv, visitor, collector));
-		}
-	}
-
-	private static void validateInvokedMethod(MethodInvocation node, ASTVisitor visitor, ProblemCollector collector) {
-		try {
-			IMethodBinding binding = node.resolveMethodBinding();
-			ICompilationUnit unit = (ICompilationUnit) binding.getJavaElement().getAncestor(IJavaElement.COMPILATION_UNIT);
-			if (unit == null) {
-				return;
-			}
-			ASTParser parser = ASTParser.newParser(AST.JLS8);
-			parser.setKind(ASTParser.K_COMPILATION_UNIT);
-			parser.setSource(unit);
-			parser.setResolveBindings(true);
-			CompilationUnit cu = (CompilationUnit) parser.createAST(null);
-			MethodDeclaration decl = (MethodDeclaration) cu.findDeclaringNode(binding.getKey());
-			decl.getBody().accept(visitor);
-		} catch (NullPointerException ex) {
-			Logger.sys.debug("Method call cannot be validated.");
-		}
-	}
-
-	private static boolean isSendInvocation(MethodInvocation expression) {
+	public static boolean isSendInvocation(MethodInvocation expression) {
 		IMethodBinding mb = expression.resolveMethodBinding();
 		return mb != null && (mb.getName().equals("send") || mb.getName().equals("fromActor"))
 				&& mb.getDeclaringClass().getQualifiedName().equals(Sequence.class.getCanonicalName());
 	}
 
-	public static void checkSendInLambda(ProblemCollector collector, Expression node, ASTVisitor visitor) {
-		if (!(node instanceof MethodInvocation)) {
-			collector.report(SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), node));
-			return;
-		}
-		MethodInvocation inv = (MethodInvocation) node;
-		if (isSendInvocation(inv)) {
-			return;
-		}
-		validateInvokedMethod(inv, visitor, collector);
-	}
-
-	public static void checkEmptyPar(ProblemCollector collector, MethodInvocation node) {
-		if (node.arguments().size() == 0) {
-			collector.report(SequenceErrors.SEND_EXPECTED.create(collector.getSourceInfo(), node));
-		}
+	public static boolean isLoopNode(Statement node) {
+		return node instanceof WhileStatement || node instanceof ForStatement || node instanceof DoStatement;
 	}
 }
