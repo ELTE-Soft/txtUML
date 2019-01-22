393958f470fd658b3ab7051657c01b8e6ea715b9
Krisztián Mózsi <mozsik0@gmail.com>
2019-01-22 05:24:32 +0100

    Add test to pingpong demo As proxy objects have been introduced, the pingpong demo became testable. Related to #590.

---
+++ examples/demo/pingpong/src/pingpong/j/tests/PingPongSequenceDiagram.java
@@ -0,0 +1,51 @@
+package pingpong.j.tests;
+
+import hu.elte.txtuml.api.model.Action;
+import hu.elte.txtuml.api.model.seqdiag.ExecMode;
+import hu.elte.txtuml.api.model.seqdiag.ExecutionMode;
+import hu.elte.txtuml.api.model.seqdiag.Lifeline;
+import hu.elte.txtuml.api.model.seqdiag.Position;
+import hu.elte.txtuml.api.model.seqdiag.Proxy;
+import hu.elte.txtuml.api.model.seqdiag.Sequence;
+import hu.elte.txtuml.api.model.seqdiag.SequenceDiagram;
+import pingpong.j.model.Game;
+import pingpong.j.model.Player;
+import pingpong.j.model.Racket;
+import pingpong.j.model.signals.Ball;
+
+public class PingPongSequenceDiagram extends SequenceDiagram {
+
+	Game game;
+
+	@Position(1)
+	Proxy<Racket> r1;
+
+	@Position(2)
+	Proxy<Racket> r2;
+
+	@Override
+	public void initialize() {
+		game = Action.create(Game.class);
+		Action.start(game);
+	}
+
+	@Override
+	@ExecutionMode(ExecMode.LENIENT)
+	public void run() {
+		Lifeline<Game> g = Sequence.createLifeline(game);
+
+		Proxy<Player> p1 = Sequence.createProxy(Player.class);
+		r1 = Sequence.createProxy(Racket.class);
+		r2 = Sequence.createProxy(Racket.class);
+
+		Sequence.fromActor(new Ball(4), g);
+		Sequence.send(g, new Ball(4), p1);
+
+		Sequence.send(r1, new Ball(4), r2);
+		Sequence.send(r2, new Ball(3), r1);
+		Sequence.send(r1, new Ball(2), r2);
+		Sequence.send(r2, new Ball(1), r1);
+		Sequence.send(r1, new Ball(0), r2);
+	}
+
+}
