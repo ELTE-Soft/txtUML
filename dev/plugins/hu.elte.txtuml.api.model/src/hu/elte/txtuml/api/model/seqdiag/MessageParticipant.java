3012ebcde53143bd54d3eaadc38b0c8b55d5d2f0
Krisztián Mózsi <mozsik0@gmail.com>
2019-02-19 17:02:20 +0100

    Introduce Proxy, Lifeline and MessageParticipant abstraction A new createProxy method is provided by Sequence API to create dummy objects. Lifeline definitions are becoming explicit in the diagram descriptions via Lifeline objects. Related to #590.

---
+++ dev/plugins/hu.elte.txtuml.api.model/src/hu/elte/txtuml/api/model/seqdiag/MessageParticipant.java
@@ -0,0 +1,44 @@
+package hu.elte.txtuml.api.model.seqdiag;
+
+import java.util.Optional;
+
+import hu.elte.txtuml.api.model.ModelClass;
+
+public class MessageParticipant<T extends ModelClass> implements Lifeline<T>, Proxy<T> {
+
+	private Optional<T> participant;
+	private Class<T> cls;
+
+	private MessageParticipant(T instance) {
+		participant = Optional.ofNullable(instance);
+	}
+
+	private MessageParticipant(Class<T> modelClass) {
+		cls = modelClass;
+		participant = Optional.empty();
+	}
+
+	public static <T extends ModelClass> MessageParticipant<T> create(T instance) {
+		return new MessageParticipant<>(instance);
+	}
+
+	public static <T extends ModelClass> MessageParticipant<T> createUnbinded(Class<T> modelClass) {
+		return new MessageParticipant<>(modelClass);
+	}
+
+	@SuppressWarnings("unchecked")
+	public void bindTo(ModelClass instance) {
+		if (instance != null && instance.getClass().equals(cls)) {
+			participant = Optional.ofNullable((T) instance);
+		}
+	}
+
+	public Optional<T> getParticipant() {
+		return participant;
+	}
+
+	public boolean hasParticipant() {
+		return participant.isPresent();
+	}
+
+}
