fb6faad8f6e7f55d07d504828e4b8e1baa999f84
Krisztián Mózsi <mozsik0@gmail.com>
2019-02-24 21:58:11 +0100

    Fix unit tests because of explicit lifeline declarations Related to #590.

--- examples/tests/hu.elte.txtuml.examples.validation/src/hu/elte/txtuml/examples/validation/sequencediagram/BaseSequence.java
+++ examples/tests/hu.elte.txtuml.examples.validation/src/hu/elte/txtuml/examples/validation/sequencediagram/BaseSequence.java
@@ -3,6 +3,8 @@ package hu.elte.txtuml.examples.validation.sequencediagram;
 import hu.elte.txtuml.api.model.Action;
 import hu.elte.txtuml.api.model.seqdiag.Position;
 import hu.elte.txtuml.api.model.seqdiag.SequenceDiagram;
+import hu.elte.txtuml.api.model.seqdiag.Sequence;
+import hu.elte.txtuml.api.model.seqdiag.Lifeline;
 import hu.elte.txtuml.examples.validation.sequencediagram.testmodel.A;
 import hu.elte.txtuml.examples.validation.sequencediagram.testmodel.AToB;
 import hu.elte.txtuml.examples.validation.sequencediagram.testmodel.B;
@@ -10,15 +12,17 @@ import hu.elte.txtuml.examples.validation.sequencediagram.testmodel.B;
 public abstract class BaseSequence extends SequenceDiagram {
 
 	@Position(10)
-	protected A lifeline1;
+	protected Lifeline<A> lifeline1;
 
-	public B lifeline2;
+	public Lifeline<B> lifeline2;
 
 	@Override
 	public void initialize() {
-		lifeline1 = Action.create(A.class);
-		lifeline2 = new B();
-		Action.link(AToB.ASide.class, lifeline1, AToB.BSide.class, lifeline2);
+		A a = Action.create(A.class);
+		B b = new B();
+		Action.link(AToB.ASide.class, a, AToB.BSide.class, b);
+		lifeline1 = Sequence.createLifeline(a);
+		lifeline2 = Sequence.createLifeline(b);
 	}
 
 }
