658cdd2f28e950f06bddccbac189c99f72a25dfa
berpeti <peti.ber007@gmail.com>
2019-04-06 15:38:26 +0200

    Create shortcut for J-style executions
    With this shortcut we are able to launch J-style executions as txtUML
    Applications from the Run As option.

---
+++ dev/plugins/hu.elte.txtuml.diagnostics/src/hu/elte/txtuml/diagnostics/launching/ExecutionAsTxtUMLAppLaunchShortcut.java
@@ -0,0 +1,12 @@
+package hu.elte.txtuml.diagnostics.launching;
+
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunchConfigurationType;
+import org.eclipse.jdt.debug.ui.launchConfigurations.JavaApplicationLaunchShortcut;
+
+public class ExecutionAsTxtUMLAppLaunchShortcut extends JavaApplicationLaunchShortcut {
+	@Override
+	protected ILaunchConfigurationType getConfigurationType() {
+		return DebugPlugin.getDefault().getLaunchManager().getLaunchConfigurationTypes()[23];
+	}
+}
