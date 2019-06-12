4f5a6d18c69f99c76b1a68eec2c0ebfe79b18e9d
András Vadász <csagataj2@gmail.com>
2019-06-12 17:23:54 +0200

    Quick fixes after merge from master

--- examples/demo/car/src/car/j/tests/CarSequenceDiagram.java
+++ examples/demo/car/src/car/j/tests/CarSequenceDiagram.java
@@ -4,6 +4,8 @@ import static hu.elte.txtuml.api.model.seqdiag.Sequence.assertState;
 import static hu.elte.txtuml.api.model.seqdiag.Sequence.fromActor;
 import static hu.elte.txtuml.api.model.seqdiag.Sequence.send;
 
+import hu.elte.txtuml.api.model.seqdiag.Lifeline;
+
 import car.x.model.Car;
 import car.x.model.ChangeGear;
 import car.x.model.ChangeSpeed;
@@ -15,31 +17,35 @@ import hu.elte.txtuml.api.model.Action;
 import hu.elte.txtuml.api.model.seqdiag.ExecMode;
 import hu.elte.txtuml.api.model.seqdiag.ExecutionMode;
 import hu.elte.txtuml.api.model.seqdiag.Position;
+import hu.elte.txtuml.api.model.seqdiag.Sequence;
 import hu.elte.txtuml.api.model.seqdiag.SequenceDiagram;
 
 public class CarSequenceDiagram extends SequenceDiagram {
 
 	// A Car will be in the first position
 	@Position(1)
-	Car car;
+	Lifeline<Car> car;
 
 	// A Gearbox will be in the second position
 	@Position(2)
-	Gearbox gearbox;
+	Lifeline<Gearbox> gearbox;
 
 	// Initialize the car and the gearbox
 	@Override
 	public void initialize() {
 		// Create the objects
-		gearbox = Action.create(Gearbox.class);
-		car = Action.create(Car.class);
+		Gearbox gearboxInstance = Action.create(Gearbox.class);
+		Car carInstance = Action.create(Car.class);
 
 		// Link the car and the gearbox via the GearboxCar association
-		Action.link(GearboxCar.g.class, gearbox, GearboxCar.c.class, car);
+		Action.link(GearboxCar.g.class, gearboxInstance, GearboxCar.c.class, carInstance);
 
+		gearbox = Sequence.createLifeline(gearboxInstance);
+		car = Sequence.createLifeline(carInstance);
+		
 		// Start the car and the gearbox
-		Action.start(car);
-		Action.start(gearbox);
+		Action.start(carInstance);
+		Action.start(gearboxInstance);
 	}
 
 	@Override
